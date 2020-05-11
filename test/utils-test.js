11a74843e69ddf86d0b7d12b124b688d4603110e
Alois Cochard <alois.cochard@gmail.com>
2020-05-11 10:17:46 +0200

    Support multiple wallets and use dedicated folder.

--- test/utils-test.js
+++ test/utils-test.js
@@ -7,8 +7,8 @@ const store = utils.Storage();
 describe('utils', function() {
   it('should store and load locally', () => {
     const obj0 = { 'theAnswer': 42 };
-    store.save(obj0);
-    const obj1 = store.load();
+    store.save('default', obj0);
+    const obj1 = store.load('default');
     assert.deepStrictEqual(obj0, obj1);
   });
 });
