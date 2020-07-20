aa6a6e08287b20d16ab5ee5257910e23fc2e3405
Alois Cochard <alois.cochard@gmail.com>
2020-07-20 15:53:47 +0200

    Properly hash seed using hmacSHA512, add fixtures from BIP32.

--- test/wallet-test.js
+++ test/wallet-test.js
@@ -13,9 +13,12 @@ describe('Wallet', function() {
   });
   it('should import wallet in a compatible manner', () => {
     let myWallet = wallet.import('absent man bright miss adult input hole useless twist chair kiss motor');
-    assert.deepStrictEqual('12DrN5ufyGFaX2nhJrrUQUZFFwQPkxVRzgsfnU9BR82ux', myWallet.address);
-    assert.deepStrictEqual('5f78d6c00ea6b4b6721f833893b549a63a020327d77202a19f058e5dcd627be6', myWallet.publicKey);
-    assert.deepStrictEqual('c9e596165497cbe6b6fe6860c3bde14bfdba8d2600f85bb9e72ade3bdeb62a64', myWallet.privateKey);
-
+    assert.deepStrictEqual('15MeQQNJuATC4GrH9u4HVJG5iSgkgxrarfVKzJ2ywtCM6', myWallet.address);
+    assert.deepStrictEqual('5819c0aae89a07b9733c9c9776fb8448aed7986681582cf43cdd2c05f0ecd1c2', myWallet.publicKey);
+    assert.deepStrictEqual('5912fe4753c447eb458c80918091ab35ddebaaaaebabefcd6c85ef3c23c13d42', myWallet.privateKey);
+  });
+  it('should generate wallet from seed in a bip32 compatible manner', () => {
+    let myWallet = wallet.fromSeed(Buffer.from('000102030405060708090a0b0c0d0e0f', 'hex'));
+    assert.deepStrictEqual('e8f32e723decf4051aefac8e2c93c9c5b214313817cdb01a1494b917c8436b35', myWallet.privateKey);
   })
 });
