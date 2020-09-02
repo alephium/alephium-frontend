c44b123aadbd269e4304924948cf09820cf60a3b
Alois Cochard <alois.cochard@gmail.com>
2020-09-02 16:59:19 +0200

    Integrate `BIP-32` and update fixture.

--- test/wallet-test.js
+++ test/wallet-test.js
@@ -12,13 +12,13 @@ describe('Wallet', function() {
     assert.deepStrictEqual(myWallet, readWallet);
   });
   it('should import wallet in a compatible manner', () => {
-    let myWallet = wallet.import('absent man bright miss adult input hole useless twist chair kiss motor');
-    assert.deepStrictEqual('15MeQQNJuATC4GrH9u4HVJG5iSgkgxrarfVKzJ2ywtCM6', myWallet.address);
-    assert.deepStrictEqual('5819c0aae89a07b9733c9c9776fb8448aed7986681582cf43cdd2c05f0ecd1c2', myWallet.publicKey);
-    assert.deepStrictEqual('5912fe4753c447eb458c80918091ab35ddebaaaaebabefcd6c85ef3c23c13d42', myWallet.privateKey);
+    let myWallet = wallet.import('captain author fragile cart whisper process ketchup wink black traffic near industry north tiny arrange extra bone segment yard rate globe normal flower wolf');
+    assert.deepStrictEqual('19KovAqpwwo5otUcx3ZRpDtLcXXEPRTrKxr1DpbqLBA49', myWallet.address);
+    assert.deepStrictEqual('03a6c7186bdde625959c70e8018a42a3c9139ce11ccfde7cbb6a038f56f1d03507', myWallet.publicKey);
+    assert.deepStrictEqual('ee0eb3a41ddfdbef696abd284e277d908bd7f643a452a399933f40cea052ac0f', myWallet.privateKey);
   });
   it('should generate wallet from seed in a bip32 compatible manner', () => {
     let myWallet = wallet.fromSeed(Buffer.from('000102030405060708090a0b0c0d0e0f', 'hex'));
-    assert.deepStrictEqual('e8f32e723decf4051aefac8e2c93c9c5b214313817cdb01a1494b917c8436b35', myWallet.privateKey);
+    assert.deepStrictEqual('582faca65228efd4cbf229a274c4811dd9e20fc5a49dca98d093e781b9f16bff', myWallet.privateKey);
   })
 });
