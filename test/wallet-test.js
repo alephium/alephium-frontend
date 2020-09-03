b8324efe2596c237090810682316acbfe746f360
Alois Cochard <alois.cochard@gmail.com>
2020-09-03 10:13:38 +0200

    Replace `keccak` with `blake2b` when hashing addresses.

--- test/wallet-test.js
+++ test/wallet-test.js
@@ -13,7 +13,7 @@ describe('Wallet', function() {
   });
   it('should import wallet in a compatible manner', () => {
     let myWallet = wallet.import('captain author fragile cart whisper process ketchup wink black traffic near industry north tiny arrange extra bone segment yard rate globe normal flower wolf');
-    assert.deepStrictEqual('19KovAqpwwo5otUcx3ZRpDtLcXXEPRTrKxr1DpbqLBA49', myWallet.address);
+    assert.deepStrictEqual('16YbeLhyPoj8aaGNFkyQP7g32pfESvAtr7RQqNRyv9GiH', myWallet.address);
     assert.deepStrictEqual('03a6c7186bdde625959c70e8018a42a3c9139ce11ccfde7cbb6a038f56f1d03507', myWallet.publicKey);
     assert.deepStrictEqual('ee0eb3a41ddfdbef696abd284e277d908bd7f643a452a399933f40cea052ac0f', myWallet.privateKey);
   });
