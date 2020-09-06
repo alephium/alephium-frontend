8cec19ab98335ce46f9b86349a29a2e49753d932
Cheng Wang <wachmc@gmail.com>
2020-09-06 13:31:11 +0200

    Add tests for compressing signatures

--- test/wallet-test.js
+++ test/wallet-test.js
@@ -13,6 +13,7 @@ describe('Wallet', function() {
     let readWallet = await wallet.open(myPassword, myWallet.encrypt(myPassword))
     assert.deepStrictEqual(myWallet, readWallet);
   });
+
   it('should import wallet in a compatible manner', () => {
     const genesis = JSON.parse(fs.readFileSync('test/genesis.json', 'utf8'));
     genesis.forEach(function(row) {
@@ -22,10 +23,12 @@ describe('Wallet', function() {
       assert.deepStrictEqual(row.priKey, myWallet.privateKey);
     });
   });
+
   it('should generate wallet from seed in a bip32 compatible manner', () => {
     let myWallet = wallet.fromSeed(Buffer.from('000102030405060708090a0b0c0d0e0f', 'hex'));
     assert.deepStrictEqual('582faca65228efd4cbf229a274c4811dd9e20fc5a49dca98d093e781b9f16bff', myWallet.privateKey);
   });
+
   it('generate mnemonic with 24 words', () => {
     let myWallet = wallet.generate();
     assert.deepStrictEqual(myWallet.mnemonic.split(" ").length, 24);
