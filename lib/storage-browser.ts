c40b3c9e00f354c948433d1cdd048c29d92fb8d4
Ilias Trichopoulos <hlias.nop@gmail.com>
2021-11-25 16:35:13 +0100

    Use template strings instead of string concat

--- lib/storage-browser.ts
+++ lib/storage-browser.ts
@@ -22,11 +22,11 @@ class BrowserStorage {
   }
 
   remove = (name: string) => {
-    window.localStorage.removeItem(this.key + '-' + name)
+    window.localStorage.removeItem(`${this.key}-${name}`)
   }
 
   load = (name: string) => {
-    const str = window.localStorage.getItem(this.key + '-' + name)
+    const str = window.localStorage.getItem(`${this.key}-${name}`)
     if (str) {
       return JSON.parse(str)
     } else {
@@ -36,7 +36,7 @@ class BrowserStorage {
 
   save = (name: string, json: unknown) => {
     const str = JSON.stringify(json)
-    window.localStorage.setItem(this.key + '-' + name, str)
+    window.localStorage.setItem(`${this.key}-${name}`, str)
   }
 
   list = () => {
