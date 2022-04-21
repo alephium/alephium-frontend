79272fdbf984520de8b4fb359fa7721c700cd2b5
mvaivre <m.vaivre@gmail.com>
2022-04-21 13:44:41 +0200

    Use LinearGradient instead of SVG on landing screen

--- src/screens/LandingScreen.tsx
+++ src/screens/LandingScreen.tsx
@@ -17,52 +17,57 @@ along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
 import { StackScreenProps } from '@react-navigation/stack'
+import { LinearGradient } from 'expo-linear-gradient'
 import { MotiView } from 'moti'
 import { StyleProp, Text, View, ViewStyle } from 'react-native'
-import styled from 'styled-components/native'
+import styled, { useTheme } from 'styled-components/native'
 
 import Button from '../components/buttons/Button'
 import ButtonStack from '../components/buttons/ButtonStack'
 import Screen from '../components/layout/Screen'
-import GradientBackground from '../images/gradients/GradientBackground'
 import AlephiumLogo from '../images/logos/AlephiumLogo'
 import RootStackParamList from '../navigation/rootStackRoutes'
 
 type ScreenProps = StackScreenProps<RootStackParamList, 'LandingScreen'>
 
-const LandingScreen = ({ navigation }: { style: StyleProp<ViewStyle> } & ScreenProps) => (
-  <Screen>
-    <LogoContainer>
-      <AlephiumLogoStyled />
-    </LogoContainer>
-    <TitleContainer>
-      <TitleFirstLine>Welcome to the official</TitleFirstLine>
-      <TitleSecondLine>Alephium Wallet</TitleSecondLine>
-    </TitleContainer>
-    <ActionsContainer>
-      <ButtonStack>
-        <Button
-          title="New wallet"
-          type="primary"
-          variant="contrast"
-          onPress={() => navigation.navigate('NewWalletIntroScreen')}
-        />
-        <Button title="Import wallet" type="secondary" variant="contrast" />
-      </ButtonStack>
-    </ActionsContainer>
-    <GradientBackgroundStyled
-      from={{ scale: 1.2 }}
-      animate={{ scale: 3 }}
-      transition={{
-        loop: true,
-        type: 'timing',
-        duration: 3000
-      }}
-    >
-      <GradientBackground />
-    </GradientBackgroundStyled>
-  </Screen>
-)
+const LandingScreen = ({ navigation }: { style: StyleProp<ViewStyle> } & ScreenProps) => {
+  const theme = useTheme()
+  const { yellow, orange, red, purple, cyan } = theme.gradient
+
+  return (
+    <Screen>
+      <LogoContainer>
+        <AlephiumLogoStyled />
+      </LogoContainer>
+      <TitleContainer>
+        <TitleFirstLine>Welcome to the official</TitleFirstLine>
+        <TitleSecondLine>Alephium Wallet</TitleSecondLine>
+      </TitleContainer>
+      <ActionsContainer>
+        <ButtonStack>
+          <Button
+            title="New wallet"
+            type="primary"
+            variant="contrast"
+            onPress={() => navigation.navigate('NewWalletIntroScreen')}
+          />
+          <Button title="Import wallet" type="secondary" variant="contrast" />
+        </ButtonStack>
+      </ActionsContainer>
+      <GradientBackgroundStyled
+        from={{ scale: 1 }}
+        animate={{ scale: 2 }}
+        transition={{
+          loop: true,
+          type: 'timing',
+          duration: 2000
+        }}
+      >
+        <LinearGradient colors={[yellow, orange, red, purple, cyan]} style={{ width: '100%', height: '100%' }} />
+      </GradientBackgroundStyled>
+    </Screen>
+  )
+}
 
 export default styled(LandingScreen)`
   flex: 1;
