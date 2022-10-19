c6d7a4c6ef4e581ec08f968338b660e1a145eb34
Ilias Trichopoulos <hlias.nop@gmail.com>
2022-10-19 11:36:04 +0200

    Use FlatList for Transfers screen

--- src/screens/TransfersScreen.tsx
+++ src/screens/TransfersScreen.tsx
@@ -17,17 +17,20 @@ along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
 import { StackScreenProps } from '@react-navigation/stack'
+import styled, { useTheme } from 'styled-components/native'
 
+import AppText from '../components/AppText'
 import DefaultHeader, { DefaultHeaderProps } from '../components/headers/DefaultHeader'
-import InWalletScrollScreen from '../components/layout/InWalletScrollScreen'
-import { ScreenSection } from '../components/layout/Screen'
-import TransactionsList from '../components/TransactionsList'
+import InWalletFlatList from '../components/layout/InWalletFlatList'
+import { ScreenSectionTitle } from '../components/layout/Screen'
+import TransactionRow from '../components/TransactionRow'
 import useInWalletTabScreenHeader from '../hooks/layout/useInWalletTabScreenHeader'
 import { useAppSelector } from '../hooks/redux'
 import InWalletTabsParamList from '../navigation/inWalletRoutes'
 import RootStackParamList from '../navigation/rootStackRoutes'
-import { selectAddressIds } from '../store/addressesSlice'
+import { selectAddressIds, selectTransactions } from '../store/addressesSlice'
 import { AddressHash } from '../types/addresses'
+import { AddressTransaction } from '../types/transactions'
 
 type ScreenProps = StackScreenProps<InWalletTabsParamList & RootStackParamList, 'TransfersScreen'>
 
@@ -35,17 +38,56 @@ const TransfersScreenHeader = (props: Partial<DefaultHeaderProps>) => (
   <DefaultHeader HeaderLeft="Transfers" {...props} />
 )
 
+const transactionKeyExtractor = (tx: AddressTransaction) => `${tx.hash}-${tx.address.hash}-${tx.blockHash}`
+
 const TransfersScreen = ({ navigation }: ScreenProps) => {
   const addressHashes = useAppSelector(selectAddressIds) as AddressHash[]
+  const txs = useAppSelector((state) => selectTransactions(state, addressHashes))
   const updateHeader = useInWalletTabScreenHeader(TransfersScreenHeader, navigation)
+  const theme = useTheme()
+
+  const renderTransactionItem = ({ item: tx, index }: { item: AddressTransaction; index: number }) => (
+    <TransactionRowStyled
+      key={transactionKeyExtractor(tx)}
+      tx={tx}
+      isFirst={index === 0}
+      isLast={index === txs.length - 1}
+    />
+  )
 
   return (
-    <InWalletScrollScreen onScroll={updateHeader}>
-      <ScreenSection>
-        <TransactionsList addressHashes={addressHashes} />
-      </ScreenSection>
-    </InWalletScrollScreen>
+    <InWalletFlatList
+      data={txs}
+      renderItem={renderTransactionItem}
+      keyExtractor={transactionKeyExtractor}
+      onScrollYChange={updateHeader}
+      initialNumToRender={8}
+      contentContainerStyle={{ flexGrow: 1 }}
+      ListHeaderComponent={<ScreenSectionTitleStyled>Latest transactions</ScreenSectionTitleStyled>}
+      ListEmptyComponent={
+        <Placeholder>
+          <AppText color={theme.font.tertiary} bold>
+            No transactions yet
+          </AppText>
+        </Placeholder>
+      }
+    />
   )
 }
 
 export default TransfersScreen
+
+const ScreenSectionTitleStyled = styled(ScreenSectionTitle)`
+  margin-left: 28px;
+  margin-top: 22px;
+`
+
+const TransactionRowStyled = styled(TransactionRow)`
+  margin: 0 20px;
+`
+
+const Placeholder = styled.View`
+  flex: 1;
+  margin-top: 200px;
+  align-items: center;
+`
