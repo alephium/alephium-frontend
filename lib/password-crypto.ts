1844ddbf72e1105cc05d82c86d693b1a00539c14
Ilias Trichopoulos <hlias.nop@gmail.com>
2022-11-26 17:22:43 +0100

    Implement simple encrypt/decrypt functions
    To be used cross platform (NodeJS/React Native)

--- lib/password-crypto.ts
+++ lib/password-crypto.ts
@@ -25,11 +25,10 @@ const authTagLength = 16
 export type Pbkdf2Function = (password: string, salt: Buffer) => Promise<Buffer>
 
 export const encrypt = (password: string, dataRaw: string): string => {
-  const data = Buffer.from(dataRaw, 'utf8')
-  const salt = randomBytes(saltByteLength)
+  const { data, salt } = formatDataAndGetRandomSalt(dataRaw)
   const derivedKey = keyFromPassword(password, salt)
 
-  return _encrypt(data, salt, derivedKey)
+  return encryptWithSalt(data, salt, derivedKey)
 }
 
 export const encryptAsync = async (
@@ -37,39 +36,32 @@ export const encryptAsync = async (
   dataRaw: string,
   pbkdf2CustomFunc: Pbkdf2Function
 ): Promise<string> => {
-  const data = Buffer.from(dataRaw, 'utf8')
-  const salt = randomBytes(saltByteLength)
+  const { data, salt } = formatDataAndGetRandomSalt(dataRaw)
   const derivedKey = await keyFromPasswordAsync(password, salt, pbkdf2CustomFunc)
 
-  return _encrypt(data, salt, derivedKey)
+  return encryptWithSalt(data, salt, derivedKey)
 }
 
-const _encrypt = (data: Buffer, salt: Buffer, derivedKey: Buffer): string => {
-  const iv = randomBytes(ivByteLength)
-  const cipher = createCipher(derivedKey, iv)
-  const encrypted = Buffer.concat([cipher.update(data), cipher.final()])
-  const authTag = cipher.getAuthTag()
-  const payload = {
-    salt: salt.toString('hex'),
-    iv: iv.toString('hex'),
-    encrypted: Buffer.concat([encrypted, authTag]).toString('hex'),
-    version: 1
-  }
+export const simpleEncrypt = (password: string, dataRaw: string): string => {
+  const { data, salt } = formatDataAndGetEmptySalt(dataRaw)
+  const derivedKey = keyFromPassword(password, salt, 'sha512')
 
-  return JSON.stringify(payload)
+  return encryptWithoutSalt(data, derivedKey)
+}
+
+export const simpleEncryptAsync = async (
+  password: string,
+  dataRaw: string,
+  pbkdf2CustomFunc: Pbkdf2Function
+): Promise<string> => {
+  const { data, salt } = formatDataAndGetEmptySalt(dataRaw)
+  const derivedKey = await pbkdf2CustomFunc(password, salt)
+
+  return encryptWithoutSalt(data, derivedKey)
 }
 
 export const decrypt = (password: string, payloadRaw: string): string => {
-  const payload = JSON.parse(payloadRaw)
-
-  const version = payload.version
-  if (version !== 1) {
-    throw new Error(`Invalid version: got ${version}, expected: 1`)
-  }
-
-  const salt = Buffer.from(payload.salt, 'hex')
-  const iv = Buffer.from(payload.iv, 'hex')
-  const encrypted = Buffer.from(payload.encrypted, 'hex')
+  const { salt, iv, encrypted } = extractDataFromPayload(payloadRaw)
   const derivedKey = keyFromPassword(password, salt)
 
   return _decrypt(iv, encrypted, derivedKey)
@@ -80,6 +72,74 @@ export const decryptAsync = async (
   payloadRaw: string,
   pbkdf2CustomFunc: Pbkdf2Function
 ): Promise<string> => {
+  const { salt, iv, encrypted } = extractDataFromPayload(payloadRaw)
+  const derivedKey = await keyFromPasswordAsync(password, salt, pbkdf2CustomFunc)
+
+  return _decrypt(iv, encrypted, derivedKey)
+}
+
+export const simpleDecrypt = (password: string, payloadRaw: string): string => {
+  const { iv, salt, encrypted } = extractDataFromSimplePayload(payloadRaw)
+  const derivedKey = keyFromPassword(password, salt)
+
+  return _decrypt(iv, encrypted, derivedKey)
+}
+
+export const simpleDecryptAsync = async (
+  password: string,
+  payloadRaw: string,
+  pbkdf2CustomFunc: Pbkdf2Function
+): Promise<string> => {
+  const { iv, salt, encrypted } = extractDataFromSimplePayload(payloadRaw)
+  const derivedKey = await keyFromPasswordAsync(password, salt, pbkdf2CustomFunc)
+
+  return _decrypt(iv, encrypted, derivedKey)
+}
+
+const encryptWithSalt = (data: Buffer, salt: Buffer, derivedKey: Buffer): string => {
+  const { iv, encrypted } = getEncryptedData(data, derivedKey)
+
+  return JSON.stringify({
+    iv,
+    encrypted,
+    salt: salt.toString('hex'),
+    version: 1
+  })
+}
+
+const encryptWithoutSalt = (data: Buffer, derivedKey: Buffer) => {
+  const { iv, encrypted } = getEncryptedData(data, derivedKey)
+
+  return JSON.stringify({
+    iv,
+    e: encrypted,
+    v: 2
+  })
+}
+
+const getEncryptedData = (data: Buffer, derivedKey: Buffer) => {
+  const iv = randomBytes(ivByteLength)
+  const cipher = createCipher(derivedKey, iv)
+  const encrypted = Buffer.concat([cipher.update(data), cipher.final()])
+  const authTag = cipher.getAuthTag()
+
+  return {
+    iv: iv.toString('hex'),
+    encrypted: Buffer.concat([encrypted, authTag]).toString('hex')
+  }
+}
+
+const formatDataAndGetRandomSalt = (dataRaw: string) => ({
+  data: Buffer.from(dataRaw, 'utf8'),
+  salt: randomBytes(saltByteLength)
+})
+
+const formatDataAndGetEmptySalt = (dataRaw: string) => ({
+  data: Buffer.from(dataRaw, 'utf8'),
+  salt: Buffer.from('', 'utf8')
+})
+
+const extractDataFromPayload = (payloadRaw: string) => {
   const payload = JSON.parse(payloadRaw)
 
   const version = payload.version
@@ -87,12 +147,26 @@ export const decryptAsync = async (
     throw new Error(`Invalid version: got ${version}, expected: 1`)
   }
 
-  const salt = Buffer.from(payload.salt, 'hex')
-  const iv = Buffer.from(payload.iv, 'hex')
-  const encrypted = Buffer.from(payload.encrypted, 'hex')
-  const derivedKey = await keyFromPasswordAsync(password, salt, pbkdf2CustomFunc)
+  return {
+    salt: Buffer.from(payload.salt, 'hex'),
+    iv: Buffer.from(payload.iv, 'hex'),
+    encrypted: Buffer.from(payload.encrypted, 'hex')
+  }
+}
 
-  return _decrypt(iv, encrypted, derivedKey)
+const extractDataFromSimplePayload = (payloadRaw: string) => {
+  const payload = JSON.parse(payloadRaw)
+
+  const version = payload.v
+  if (version !== 2) {
+    throw new Error(`Invalid version: got ${version}, expected: 2`)
+  }
+
+  return {
+    salt: Buffer.from('', 'hex'),
+    iv: Buffer.from(payload.iv, 'hex'),
+    encrypted: Buffer.from(payload.e, 'hex')
+  }
 }
 
 const _decrypt = (iv: Buffer, encrypted: Buffer, derivedKey: Buffer): string => {
@@ -113,8 +187,8 @@ const createDecipher = (key: CipherKey, iv: BinaryLike | null) => {
   return createDecipheriv('aes-256-gcm', key, iv)
 }
 
-const keyFromPassword = (password: BinaryLike, salt: BinaryLike): Buffer => {
-  return pbkdf2Sync(password, salt, 10000, 32, 'sha256')
+const keyFromPassword = (password: BinaryLike, salt: BinaryLike, digest: 'sha256' | 'sha512' = 'sha256'): Buffer => {
+  return pbkdf2Sync(password, salt, 10000, 32, digest)
 }
 
 const keyFromPasswordAsync = (password: string, salt: Buffer, pbkdf2CustomFunc: Pbkdf2Function): Promise<Buffer> => {
