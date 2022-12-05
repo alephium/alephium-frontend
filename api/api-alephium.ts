ccf2b46c871685b013339203476b6799e8349de3
mvaivre <m.vaivre@gmail.com>
2022-12-05 16:18:03 +0100

    Update node ver to 1.5.4

--- api/api-alephium.ts
+++ api/api-alephium.ts
@@ -471,7 +471,7 @@ export interface CompilerOptions {
   ignoreUnusedVariablesWarnings?: boolean
   ignoreUnusedFieldsWarnings?: boolean
   ignoreUnusedPrivateFunctionsWarnings?: boolean
-  ignoreReadonlyCheckWarnings?: boolean
+  ignoreUpdateFieldsCheckWarnings?: boolean
   ignoreExternalCallCheckWarnings?: boolean
 }
 
@@ -1332,7 +1332,7 @@ export class HttpClient<SecurityDataType = unknown> {
 
 /**
  * @title Alephium API
- * @version 1.5.0
+ * @version 1.5.4
  * @baseUrl ../
  */
 export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDataType> {
