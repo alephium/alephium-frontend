7808d0d29ceb56dbed590e4c22209d856a46d92a
Ilias Trichopoulos <hlias.nop@gmail.com>
2023-08-14 11:13:29 +0200

    Fix overlap of modal close button
    Closes #176

--- src/components/QRCodeScannerModal.tsx
+++ src/components/QRCodeScannerModal.tsx
@@ -18,6 +18,7 @@ along with the library. If not, see <http://www.gnu.org/licenses/>.
 
 import { BarCodeScanner } from 'expo-barcode-scanner'
 import { BarCodeScanningResult, Camera, CameraType } from 'expo-camera'
+import ExpoConstants from 'expo-constants'
 import { Camera as CameraIcon, X } from 'lucide-react-native'
 import { areFramesComplete, framesToData, parseFramesReducer, progressOfFrames, State as FrameState } from 'qrloop'
 import React, { useEffect, useState } from 'react'
@@ -142,6 +143,7 @@ export default QRCodeScannerModal
 
 const ScreenStyled = styled(Screen)`
   width: 100%;
+  padding-top: ${ExpoConstants.statusBarHeight}px;
 `
 
 const CloseButton = styled.Pressable`
