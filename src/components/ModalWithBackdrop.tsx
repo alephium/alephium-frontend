12f2f9f5674d375fbc23ad749cee07858d1a16fc
mvaivre <m.vaivre@gmail.com>
2023-09-26 17:18:18 +0200

    Enforce animationType="none" in ModalWithBackdrop
    In order to avoid iOS bug where the fading in animation didn't get triggered...!

--- src/components/ModalWithBackdrop.tsx
+++ src/components/ModalWithBackdrop.tsx
@@ -25,7 +25,7 @@ interface ModalWithBackdropProps extends ModalProps {
 }
 
 const ModalWithBackdrop = ({ children, closeModal, color, ...props }: ModalWithBackdropProps) => (
-  <Modal transparent={true} {...props}>
+  <Modal transparent={true} {...props} animationType="none">
     <ModalBackdrop onPress={closeModal} color={color} />
     <ModalContent>{children}</ModalContent>
   </Modal>
