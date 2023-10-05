ecd2c5f7e9f6360b49332c5e33d5b3fcfee760df
mvaivre <m.vaivre@gmail.com>
2023-10-05 16:51:40 +0200

    Add default address toggle

--- src/components/AddressCard.tsx
+++ src/components/AddressCard.tsx
@@ -21,17 +21,22 @@ import { NavigationProp, useNavigation } from '@react-navigation/native'
 import { colord } from 'colord'
 import { LinearGradient } from 'expo-linear-gradient'
 import { usePostHog } from 'posthog-react-native'
+import { useState } from 'react'
 import { StyleProp, View, ViewStyle } from 'react-native'
+import Toast from 'react-native-root-toast'
 import styled, { useTheme } from 'styled-components/native'
 
 import AddressBadge from '~/components/AddressBadge'
 import Amount from '~/components/Amount'
+import AppText from '~/components/AppText'
 import Button from '~/components/buttons/Button'
 import ButtonsRow from '~/components/buttons/ButtonsRow'
-import { useAppSelector } from '~/hooks/redux'
+import SpinnerModal from '~/components/SpinnerModal'
+import usePersistAddressSettings from '~/hooks/layout/usePersistAddressSettings'
+import { useAppDispatch, useAppSelector } from '~/hooks/redux'
 import DefaultAddressBadge from '~/images/DefaultAddressBadge'
 import { SendNavigationParamList } from '~/navigation/SendNavigation'
-import { selectAddressByHash } from '~/store/addressesSlice'
+import { addressSettingsSaved, selectAddressByHash } from '~/store/addressesSlice'
 import { useGetPriceQuery } from '~/store/assets/priceApiSlice'
 import { DEFAULT_MARGIN } from '~/style/globalStyle'
 import { AddressHash } from '~/types/addresses'
@@ -45,10 +50,15 @@ interface AddressCardProps {
 
 const AddressCard = ({ style, addressHash, onSettingsPress }: AddressCardProps) => {
   const theme = useTheme()
+  const dispatch = useAppDispatch()
   const navigation = useNavigation<NavigationProp<SendNavigationParamList>>()
   const posthog = usePostHog()
   const address = useAppSelector((s) => selectAddressByHash(s, addressHash))
   const currency = useAppSelector((s) => s.settings.currency)
+  const persistAddressSettings = usePersistAddressSettings()
+
+  const [loading, setLoading] = useState(false)
+
   const totalAddressBalance = BigInt(address?.balance ?? 0) + BigInt(address?.lockedBalance ?? 0)
   const { data: price } = useGetPriceQuery(currencies[currency].ticker, {
     pollingInterval: 60000,
@@ -56,13 +66,14 @@ const AddressCard = ({ style, addressHash, onSettingsPress }: AddressCardProps) 
   })
 
   const totalAmountWorth = calculateAmountWorth(totalAddressBalance, price ?? 0)
+  const isDefaultAddress = address?.settings.isDefault
 
   if (!address) return null
 
   const bgColor = address.settings.color ?? theme.font.primary
   const textColor = colord(bgColor).isDark() ? 'white' : 'black'
   const outterBorderColor = colord(bgColor).lighten(0.15).toHex()
-  const innerBorderColor = colord(bgColor).lighten(0.05).toHex()
+  const innerBorderColor = colord(bgColor).lighten(0.02).toHex()
 
   const handleSendPress = () => {
     posthog?.capture('Address card: Selected address to send funds from')
@@ -80,6 +91,26 @@ const AddressCard = ({ style, addressHash, onSettingsPress }: AddressCardProps) 
       screen: 'QRCodeScreen',
       params: { addressHash }
     })
+  }
+
+  const handleDefaultAddressToggle = async () => {
+    setLoading(true)
+
+    try {
+      const newSettings = { ...address.settings, isDefault: true }
+
+      await persistAddressSettings({ ...address, settings: newSettings })
+      dispatch(addressSettingsSaved({ ...address, settings: newSettings }))
+
+      Toast.show('This is now the default address')
+
+      posthog?.capture('Address: Used address card default toggle')
+    } catch (e) {
+      console.error(e)
+
+      posthog?.capture('Error', { message: 'Could not use address card default toggle' })
+    }
+    setLoading(false)
   }
 
   return (
@@ -112,9 +143,15 @@ const AddressCard = ({ style, addressHash, onSettingsPress }: AddressCardProps) 
           </AddressBadgeContainer>
           <HeaderButtons>
             <Button
-              color="white"
-              onPress={onSettingsPress}
-              customIcon={<DefaultAddressBadge size={18} color={textColor} />}
+              onPress={handleDefaultAddressToggle}
+              customIcon={
+                <DefaultAddressBadge
+                  strokeOnly={!isDefaultAddress}
+                  size={18}
+                  color={isDefaultAddress ? theme.global.accent : textColor}
+                />
+              }
+              style={isDefaultAddress ? { backgroundColor: 'rgba(255, 255, 255, 0.5)' } : undefined}
               round
             />
             <Button iconProps={{ name: 'settings-outline' }} color={textColor} onPress={onSettingsPress} round />
@@ -130,6 +167,11 @@ const AddressCard = ({ style, addressHash, onSettingsPress }: AddressCardProps) 
             suffix={currencies[currency].symbol}
           />
           <Amount value={BigInt(address.balance)} color={textColor} size={15} medium suffix="ALPH" />
+          <AddressGroup>
+            <AppText style={{ color: textColor }} size={13}>
+              Group {address.group}
+            </AppText>
+          </AddressGroup>
         </Amounts>
         <BottomRow style={{ borderTopColor: innerBorderColor }}>
           <ButtonsRow sticked hasDivider dividerColor={innerBorderColor}>
@@ -152,6 +194,7 @@ const AddressCard = ({ style, addressHash, onSettingsPress }: AddressCardProps) 
           </ButtonsRow>
         </BottomRow>
       </CardGradientContainer>
+      <SpinnerModal isActive={loading} text="Changing default address..." />
     </View>
   )
 }
@@ -203,6 +246,13 @@ const Amounts = styled.View`
 
 const FiatAmount = styled(Amount)`
   margin-bottom: 5px;
+`
+
+const AddressGroup = styled.View`
+  position: absolute;
+  right: 15px;
+  bottom: 0px;
+  opacity: 0.6;
 `
 
 const BottomRow = styled.View`
--- src/images/DefaultAddressBadge.tsx
+++ src/images/DefaultAddressBadge.tsx
@@ -16,22 +16,31 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { Canvas, FitBox, Path, rect } from '@shopify/react-native-skia'
+import { Canvas, FitBox, Group, Path, rect } from '@shopify/react-native-skia'
 import { StyleProp, ViewStyle } from 'react-native'
 
 interface DefaultAddressBadgeProps {
   style?: StyleProp<ViewStyle>
   color?: string
+  strokeOnly?: boolean
   size?: number
 }
 
-const DefaultAddressBadge = ({ style, size = 24, color = 'black' }: DefaultAddressBadgeProps) => (
-  <Canvas style={[style, { width: size, height: size }]}>
+const DefaultAddressBadge = ({ style, size = 24, color = 'black', strokeOnly }: DefaultAddressBadgeProps) => (
+  <Canvas style={[style, { width: size + 2, height: size + 2 }]}>
     <FitBox src={rect(0, 0, 24, 24)} dst={rect(0, 0, size, size)}>
-      <Path
-        color={color}
-        path="M24,12c0,-1.674 -1.03,-3.126 -2.564,-3.91c0.539,-1.628 0.234,-3.395 -0.948,-4.578c-1.183,-1.182 -2.95,-1.487 -4.578,-0.948c-0.772,-1.534 -2.236,-2.564 -3.91,-2.564c-1.674,0 -3.126,1.03 -3.899,2.564c-1.639,-0.539 -3.406,-0.234 -4.589,0.948c-1.182,1.183 -1.475,2.95 -0.936,4.578c-1.534,0.784 -2.576,2.236 -2.576,3.91c0,1.674 1.042,3.126 2.576,3.91c-0.539,1.628 -0.246,3.395 0.936,4.578c1.183,1.182 2.95,1.475 4.578,0.948c0.784,1.534 2.236,2.564 3.91,2.564c1.674,0 3.138,-1.03 3.91,-2.564c1.628,0.527 3.395,0.234 4.578,-0.948c1.182,-1.183 1.487,-2.95 0.948,-4.578c1.534,-0.784 2.564,-2.236 2.564,-3.91Zm-13.709,4.917l-4.379,-4.378l1.651,-1.663l2.646,2.646l5.619,-6.123l1.721,1.592l-7.258,7.926Z"
-      />
+      <Group transform={[{ translateX: 1 }, { translateY: 1 }]}>
+        <Path
+          color={color}
+          path="M24,12c0,-1.674 -1.03,-3.126 -2.564,-3.91c0.539,-1.628 0.234,-3.395 -0.948,-4.578c-1.183,-1.182 -2.95,-1.487 -4.578,-0.948c-0.772,-1.534 -2.236,-2.564 -3.91,-2.564c-1.674,0 -3.126,1.03 -3.899,2.564c-1.639,-0.539 -3.406,-0.234 -4.589,0.948c-1.182,1.183 -1.475,2.95 -0.936,4.578c-1.534,0.784 -2.576,2.236 -2.576,3.91c0,1.674 1.042,3.126 2.576,3.91c-0.539,1.628 -0.246,3.395 0.936,4.578c1.183,1.182 2.95,1.475 4.578,0.948c0.784,1.534 2.236,2.564 3.91,2.564c1.674,0 3.138,-1.03 3.91,-2.564c1.628,0.527 3.395,0.234 4.578,-0.948c1.182,-1.183 1.487,-2.95 0.948,-4.578c1.534,-0.784 2.564,-2.236 2.564,-3.91Z"
+          style={strokeOnly ? 'stroke' : 'fill'}
+          strokeWidth={strokeOnly ? 2 : 0}
+        />
+        <Path
+          color={!strokeOnly ? 'white' : color}
+          path="M24 12m-13.709 4.917-4.379-4.378 1.651-1.663 2.646 2.646 5.619-6.123 1.721 1.592-7.258 7.926Z"
+        />
+      </Group>
     </FitBox>
   </Canvas>
 )
