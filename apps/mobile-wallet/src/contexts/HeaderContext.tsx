bc52de1e24e1e0903f6699c1d164abd8ba780c5e
mvaivre <m.vaivre@gmail.com>
2023-11-21 18:44:27 +0100

    Pass scroll SharedValue through context
    But issue with navigator overflow not working..!

--- apps/mobile-wallet/src/contexts/HeaderContext.tsx
+++ apps/mobile-wallet/src/contexts/HeaderContext.tsx
@@ -17,29 +17,42 @@ along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
 import { createContext, ReactNode, useContext, useState } from 'react'
+import { NativeScrollEvent, NativeSyntheticEvent } from 'react-native'
+import { SharedValue, useSharedValue } from 'react-native-reanimated'
 
 import { BaseHeaderOptions } from '~/components/headers/BaseHeader'
 
 interface HeaderContextValue {
   headerOptions: BaseHeaderOptions
   setHeaderOptions: (options: BaseHeaderOptions) => void
+  screenScrollY?: SharedValue<number>
+  screenScrollHandler: (e: NativeSyntheticEvent<NativeScrollEvent>) => void
 }
 
 const initialValues: HeaderContextValue = {
   headerOptions: {},
-  setHeaderOptions: () => null
+  setHeaderOptions: () => null,
+  screenScrollY: undefined,
+  screenScrollHandler: () => null
 }
 
 const HeaderContext = createContext(initialValues)
 
 export const HeaderContextProvider = ({ children }: { children: ReactNode }) => {
   const [headerOptions, setHeaderOptions] = useState<HeaderContextValue['headerOptions']>(initialValues.headerOptions)
+  const screenScrollY = useSharedValue(0)
+
+  const screenScrollHandler = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
+    screenScrollY.value = e.nativeEvent.contentOffset.y
+  }
 
   return (
     <HeaderContext.Provider
       value={{
         headerOptions,
-        setHeaderOptions
+        setHeaderOptions,
+        screenScrollY,
+        screenScrollHandler
       }}
     >
       {children}
