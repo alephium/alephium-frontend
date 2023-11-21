1f6300d1f8539c4ab9d709f0bc9b5e756c3ce655
Ilias Trichopoulos <hlias.nop@gmail.com>
2023-11-21 12:32:28 +0700

    Update iOS deployment target to 13.4 to fix build

--- apps/mobile-wallet/app.config.js
+++ apps/mobile-wallet/app.config.js
@@ -57,7 +57,7 @@ export default {
         'expo-build-properties',
         {
           ios: {
-            deploymentTarget: '13.0',
+            deploymentTarget: '13.4',
             newArchEnabled: false,
             flipper: true
           },
