6ae1216ade3d29dfb7590c89713c0a32ae4d6ee4
mvaivre <m.vaivre@gmail.com>
2023-11-30 10:45:04 +0100

    Rework Modal anims, use side modal.

--- apps/explorer/src/components/Modal/Modal.tsx
+++ apps/explorer/src/components/Modal/Modal.tsx
@@ -35,16 +35,16 @@ const Modal = ({ isOpen = false, onClose, children, className, maxWidth = 600 }:
       <ModalWrapper>
         <Backdrop
           onClick={onClose}
-          initial={{ backdropFilter: 'blur(0px)', opacity: 0 }}
-          animate={{ backdropFilter: 'blur(5px)', opacity: 1 }}
-          exit={{ backdropFilter: 'blur(0px)', opacity: 0 }}
-          transition={{ duration: 0.2 }}
+          initial={{ WebkitBackdropFilter: 'blur(0px)', backdropFilter: 'blur(0px)', opacity: 0 }}
+          animate={{ WebkitBackdropFilter: 'blur(3px)', backdropFilter: 'blur(3px)', opacity: 1 }}
+          exit={{ WebkitBackdropFilter: 'blur(0px)', backdropFilter: 'blur(0px)', opacity: 0 }}
+          transition={{ type: 'spring', stiffness: 310, damping: 30 }}
         />
         <ModalContentWrapper
-          initial={{ opacity: 0, scale: 0.98 }}
-          animate={{ opacity: 1, scale: 1 }}
-          exit={{ opacity: 0, scale: 0.98 }}
-          transition={{ duration: 0.1 }}
+          initial={{ opacity: 0, scale: 0.98, x: '10%' }}
+          animate={{ opacity: 1, scale: 1, x: 0 }}
+          exit={{ opacity: 0, scale: 0.98, x: '10%' }}
+          transition={{ type: 'spring', stiffness: 300, damping: 40 }}
           className={className}
           style={{ maxWidth }}
         >
@@ -68,11 +68,13 @@ const ModalWrapper = styled.div`
 `
 
 const ModalContentWrapper = styled(motion.div)`
-  position: relative;
-  margin: auto;
+  position: absolute;
+  right: 25px;
+  top: 25px;
+  bottom: 25px;
   border-radius: 12px;
   border: 1px solid ${({ theme }) => theme.border.primary};
-  background-color: ${({ theme }) => theme.bg.background1};
+  background-color: ${({ theme }) => theme.bg.secondary};
   overflow-y: auto;
   z-index: 1;
   box-shadow: ${({ theme }) => theme.shadow.tertiary};
