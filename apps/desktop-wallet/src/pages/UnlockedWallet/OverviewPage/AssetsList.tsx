a7536a3c8944f787a8d4d511ff4fb52258b640ee
mvaivre <m.vaivre@gmail.com>
2024-01-03 09:54:52 +0100

    Mention that total value is only about ALPH for now

--- apps/desktop-wallet/src/pages/UnlockedWallet/OverviewPage/AssetsList.tsx
+++ apps/desktop-wallet/src/pages/UnlockedWallet/OverviewPage/AssetsList.tsx
@@ -170,7 +170,7 @@ const TokenListRow = ({ asset, isExpanded }: TokenListRowProps) => {
   const theme = useTheme()
   const stateUninitialized = useAppSelector(selectIsStateUninitialized)
   const fiatCurrency = useAppSelector((s) => s.settings.fiatCurrency)
-  const price = useAppSelector((s) => selectPriceById(s, asset.id))
+  const price = useAppSelector((s) => selectPriceById(s, asset.symbol?.toLowerCase() || ''))
 
   return (
     <TableRow key={asset.id} role="row" tabIndex={isExpanded ? 0 : -1}>
