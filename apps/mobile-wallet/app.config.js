50057258f8404e9ff05d646ccb532ed75dc4b663
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-02-19 19:47:27 +0800

    Bump mobile wallet version to 1.0.6

--- apps/mobile-wallet/app.config.js
+++ apps/mobile-wallet/app.config.js
@@ -20,7 +20,7 @@ export default {
   expo: {
     name: 'Alephium',
     slug: 'alephium-mobile-wallet',
-    version: '1.0.5',
+    version: '1.0.6',
     orientation: 'portrait',
     icon: './assets/icon.png',
     scheme: ['wc', 'alephium'],
