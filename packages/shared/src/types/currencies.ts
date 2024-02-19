01a0ee8b2c97cb267c1fcefdccdf4598df60d32a
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-02-19 19:28:23 +0800

    Add CAD
    Closes #241

--- packages/shared/src/types/currencies.ts
+++ packages/shared/src/types/currencies.ts
@@ -16,4 +16,4 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-export type Currency = 'CHF' | 'IDR' | 'GBP' | 'EUR' | 'USD' | 'VND' | 'RUB' | 'TRY'
+export type Currency = 'CHF' | 'IDR' | 'GBP' | 'EUR' | 'USD' | 'VND' | 'RUB' | 'TRY' | 'CAD'
