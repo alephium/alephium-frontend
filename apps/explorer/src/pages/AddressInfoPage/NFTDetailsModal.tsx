1e0fabd801e4ba27455dba9a9dc26803a4583c99
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-02-20 10:45:24 +0800

    Improve NFT types

--- apps/explorer/src/pages/AddressInfoPage/NFTDetailsModal.tsx
+++ apps/explorer/src/pages/AddressInfoPage/NFTDetailsModal.tsx
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { NFTCollectionUriMetaData } from '@alephium/shared'
+import { NFTCollectionUriMetaData } from '@alephium/web3'
 import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
 
