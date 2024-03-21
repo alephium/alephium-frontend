e41dd997fcfcaac15fc052b80ad9716bcfc9a367
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-03-21 09:32:17 +0900

    Disable storage clear
    Related to #464

--- apps/mobile-wallet/src/contexts/walletConnect/WalletConnectContext.tsx
+++ apps/mobile-wallet/src/contexts/walletConnect/WalletConnectContext.tsx
@@ -1012,7 +1012,7 @@ export const WalletConnectContextProvider = ({ children }: { children: ReactNode
   const resetWalletConnectStorage = useCallback(async () => {
     if (walletConnectClient === undefined) {
       console.log('Clear walletconnect storage')
-      await clearWCStorage()
+      // await clearWCStorage()
       return
     }
 
@@ -1042,7 +1042,7 @@ export const WalletConnectContextProvider = ({ children }: { children: ReactNode
       walletConnectClient.core.relayer.subscriber.subscriptions.clear()
 
       console.log('Clear walletconnect storage')
-      await clearWCStorage()
+      // await clearWCStorage()
     } catch (error) {
       sendErrorAnalytics(error, 'Error at resetting WalletConnect storage')
     }
