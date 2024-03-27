ab658f3d06a98f6b50769eb1be7b98a2762b56a4
mvaivre <m.vaivre@gmail.com>
2024-03-27 16:42:55 +0100

    Fetch NFT's collection metadata and data using RTK Query

---
+++ packages/shared/src/api/nftsApi.ts
@@ -0,0 +1,25 @@
+import { alephiumBaseQuery } from '@/api/alephiumBaseQuery'
+import { client } from '@/api/client'
+import { NFTCollectionUriMetaData, addressFromContractId } from '@alephium/web3'
+import { NFTCollectionMetadata } from '@alephium/web3/dist/src/api/api-explorer'
+import { createApi } from '@reduxjs/toolkit/query/react'
+
+export const nftsApi = createApi({
+  baseQuery: alephiumBaseQuery,
+  tagTypes: ['NFTCollectionMetadata', 'NFTCollectionUriMetaData'],
+  endpoints: (build) => ({
+    getNFTCollectionMetadata: build.query<NFTCollectionMetadata, string>({
+      queryFn: async (collectionId) => ({
+        data: (await client.explorer.tokens.postTokensNftCollectionMetadata([addressFromContractId(collectionId)]))[0]
+      }),
+      providesTags: (result, error, collectionId) => [{ type: 'NFTCollectionMetadata', collectionId }]
+    }),
+    getNFTCollectionData: build.query<NFTCollectionUriMetaData, string>({
+      queryFn: (collectionUri) =>
+        fetch(collectionUri).then((res) => ({ data: res.json() as unknown as NFTCollectionUriMetaData })),
+      providesTags: (result, error, collectionUri) => [{ type: 'NFTCollectionUriMetaData', collectionUri }]
+    })
+  })
+})
+
+export const { useGetNFTCollectionMetadataQuery, useGetNFTCollectionDataQuery } = nftsApi
