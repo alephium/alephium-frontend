b0b347c49c6a5fc5b93f1bca294ab21bd6837bae
mvaivre <m.vaivre@gmail.com>
2024-03-28 13:19:28 +0100

    Refactor DataList again, and update TransactionDetailsModal

--- apps/desktop-wallet/src/components/DataList.tsx
+++ apps/desktop-wallet/src/components/DataList.tsx
@@ -16,34 +16,47 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { ReactNode } from 'react'
+import { Children, isValidElement, ReactNode } from 'react'
 import styled from 'styled-components'
 
-export type DataListItem = {
-  label: string
-  value?: ReactNode
-}
-
 interface DataListProps {
-  items: DataListItem[]
   title?: string
+  children: ReactNode
 }
 
-const DataList = ({ items, title }: DataListProps) => (
-  <DataListStyled>
-    {title && <DataListTitle>{title}</DataListTitle>}
-    {items.map(({ label, value }) => (
-      <DataRowStyled role="row" tabIndex={0}>
-        <DetailsRowLabel tabIndex={0} role="cell">
-          {label}
-        </DetailsRowLabel>
-        <ChildrenWrapper>{value || '-'}</ChildrenWrapper>
-      </DataRowStyled>
-    ))}
-  </DataListStyled>
+const DataList = ({ children, title }: DataListProps) => {
+  // TODO: If we like this approach, let's create a hook to check children types on runtime?
+  Children.forEach(children, (child) => {
+    if (child && isValidElement(child) && child.type !== DataListRow) {
+      console.error('DataList only accepts DataList.Row as children.')
+    }
+  })
+
+  return (
+    <DataListStyled>
+      {title && <DataListTitle>{title}</DataListTitle>}
+      {children}
+    </DataListStyled>
+  )
+}
+
+export interface DataListRowProps {
+  label: string
+  children?: ReactNode
+}
+
+export const DataListRow = ({ label, children }: DataListRowProps) => (
+  <DataRowStyled role="row" tabIndex={0}>
+    <DetailsRowLabel tabIndex={0} role="cell">
+      {label}
+    </DetailsRowLabel>
+    <ChildrenWrapper>{children || '-'}</ChildrenWrapper>
+  </DataRowStyled>
 )
 
 export default DataList
+
+DataList.Row = DataListRow
 
 const DataListStyled = styled.div`
   display: flex;
@@ -58,7 +71,7 @@ const DataListTitle = styled.h2`
 `
 
 const DataRowStyled = styled.div`
-  padding: var(--spacing-4) 0;
+  padding: var(--spacing-3) 0;
   display: flex;
   gap: var(--spacing-3);
   align-items: center;
@@ -70,7 +83,7 @@ const DataRowStyled = styled.div`
 
 const DetailsRowLabel = styled.div`
   font-weight: var(--fontWeight-medium);
-  width: max(100px, 30%);
+  width: max(100px, 35%);
   color: ${({ theme }) => theme.font.secondary};
 `
 
