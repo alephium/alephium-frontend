7734ed3ad0067438e797c22c534dd225d351529a
mvaivre <m.vaivre@gmail.com>
2024-04-03 15:18:17 +0200

    Increase NFT collection data retention to 1 day

--- packages/shared/src/api/nftsApi.ts
+++ packages/shared/src/api/nftsApi.ts
@@ -1,6 +1,6 @@
 import { alephiumBaseQuery } from '@/api/alephiumBaseQuery'
 import { client } from '@/api/client'
-import { ONE_HOUR_MS } from '@/constants'
+import { ONE_DAY_MS } from '@/constants'
 import { NFTCollectionUriMetaData, addressFromContractId } from '@alephium/web3'
 import { NFTCollectionMetadata } from '@alephium/web3/dist/src/api/api-explorer'
 import { createApi } from '@reduxjs/toolkit/query/react'
@@ -13,13 +13,14 @@ export const nftsApi = createApi({
       queryFn: async (collectionId) => ({
         data: (await client.explorer.tokens.postTokensNftCollectionMetadata([addressFromContractId(collectionId)]))[0]
       }),
-      providesTags: (result, error, collectionId) => [{ type: 'NFTCollectionMetadata', collectionId }]
+      providesTags: (result, error, collectionId) => [{ type: 'NFTCollectionMetadata', collectionId }],
+      keepUnusedDataFor: ONE_DAY_MS * 1000
     }),
     getNFTCollectionData: build.query<NFTCollectionUriMetaData, string>({
       queryFn: (collectionUri) =>
         fetch(collectionUri).then((res) => ({ data: res.json() as unknown as NFTCollectionUriMetaData })),
       providesTags: (result, error, collectionUri) => [{ type: 'NFTCollectionUriMetaData', collectionUri }],
-      keepUnusedDataFor: ONE_HOUR_MS * 1000
+      keepUnusedDataFor: ONE_DAY_MS * 1000
     })
   })
 })
