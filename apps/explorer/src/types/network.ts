a2e94e8b3c1dd226592545a9d513d0df702814ce
h0ngcha0 <lbs.lhc@gmail.com>
2024-04-12 11:02:42 +0200

    Add devnet to networkTypes

--- apps/explorer/src/types/network.ts
+++ apps/explorer/src/types/network.ts
@@ -16,6 +16,6 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-export const networkTypes = ['testnet', 'mainnet'] as const
+export const networkTypes = ['devnet', 'testnet', 'mainnet'] as const
 
 export type NetworkType = (typeof networkTypes)[number]
