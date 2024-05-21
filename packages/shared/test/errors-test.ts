126c42c67d593b406aa5343a7de12f6148c940f5
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-05-21 19:11:03 +0200

    Fix tests

--- packages/shared/test/errors-test.ts
+++ packages/shared/test/errors-test.ts
@@ -20,41 +20,42 @@ import { cleanExceptionMessage } from '@/errors'
 
 describe('cleanExceptionMessage', () => {
   it('Replaces bip39 words with [...]', () => {
-    const error = 'This is an error containing abandon and ability'
-    const result = cleanExceptionMessage(error)
-    expect(result).toBe('[...] is an [...] containing [...] and [...]')
+    expect(cleanExceptionMessage(new Error('This is an error containing abandon and ability'))).toBe(
+      'Error: This is an [...] containing [...] and [...]'
+    )
   })
 
   it('Returns the same message if no bip39 words are found', () => {
-    const error = 'Here is a string with no sensitive words'
-    const result = cleanExceptionMessage(error)
-    expect(result).toBe('Here is a string with no sensitive words')
+    expect(cleanExceptionMessage(new Error('Here is a string with no sensitive words'))).toBe(
+      'Error: Here is a string with no sensitive words'
+    )
   })
 
   it('Handles empty error message', () => {
-    const error = ''
-    const result = cleanExceptionMessage(error)
-    expect(result).toBe('')
+    expect(cleanExceptionMessage(new Error(''))).toBe('Error')
   })
 
   it('Handles non-string errors', () => {
-    const error = new Error('Here is a cleaned up error object')
-    const result = cleanExceptionMessage(error)
-    expect(result).toBe('[...]: Here is a cleaned up [...] [...]')
+    expect(cleanExceptionMessage(new Error('Here is a cleaned up error object'))).toBe(
+      'Error: Here is a cleaned up [...] [...]'
+    )
   })
 
   it('Replaces words in a full 24-word mnemonic', () => {
-    const error =
-      'pumpkin price lake liar into school cotton town trial kangaroo wrist trend work slab candy napkin today scene fun answer also bid garage lottery'
-    const result = cleanExceptionMessage(error)
-    expect(result).toBe(
-      '[...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...]'
+    expect(
+      cleanExceptionMessage(
+        new Error(
+          'pumpkin price lake liar into school cotton town trial kangaroo wrist trend work slab candy napkin today scene fun answer also bid garage lottery'
+        )
+      )
+    ).toBe(
+      'Error: [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...] [...]'
     )
   })
 
   it('Replaces multiple occurrences of the same word', () => {
-    const error = 'Here is a string containing abandon, abandon, and abandon'
-    const result = cleanExceptionMessage(error)
-    expect(result).toBe('Here is a string containing [...], [...], and [...]')
+    expect(cleanExceptionMessage(new Error('Here is a string containing abandon, abandon, and abandon'))).toBe(
+      'Error: Here is a string containing [...], [...], and [...]'
+    )
   })
 })
