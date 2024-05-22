89a1ea420022a8ff67618786317ace1e17d9a600
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-05-22 14:58:30 +0200

    Handle more error message cases

--- packages/shared/src/errors.ts
+++ packages/shared/src/errors.ts
@@ -20,13 +20,18 @@ import { bip39Words } from '@/bip39'
 
 const bip39WordsString = bip39Words.join('|')
 
-export const getHumanReadableError = (error: unknown, defaultErrorMsg: string) =>
-  defaultErrorMsg +
-  (errorHasMessageProp(error)
-    ? ` - ${error.message}`
-    : typeof error?.toString === 'function'
-      ? ` - ${error.toString().replace('Error: [API Error] - ', '')}`
-      : '')
+export const getHumanReadableError = (error: unknown, defaultErrorMsg: string) => {
+  const messageChunks = []
+
+  if (defaultErrorMsg) messageChunks.push(defaultErrorMsg)
+  if (errorHasMessageProp(error) && error.message) {
+    messageChunks.push(error.message)
+  } else if (typeof error?.toString === 'function') {
+    messageChunks.push(error.toString().replace('Error: [API Error] - ', ''))
+  }
+
+  return messageChunks.join(' - ')
+}
 
 export const cleanExceptionMessage = (error: unknown) =>
   getHumanReadableError(error, '').replace(new RegExp(`\\b(${bip39WordsString})\\b`, 'g'), '[...]')
