d9cad1e7b1ae3d70880f49df534911c664fd71ae
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-06-17 16:08:50 +0200

    Enable Greek in explorer

--- apps/explorer/src/i18n.ts
+++ apps/explorer/src/i18n.ts
@@ -19,6 +19,7 @@ along with the library. If not, see <http://www.gnu.org/licenses/>.
 import i18next from 'i18next'
 import { initReactI18next } from 'react-i18next'
 
+import el from '../locales/el-GR/translation.json'
 import en from '../locales/en-US/translation.json'
 import fr from '../locales/fr-FR/translation.json'
 import id from '../locales/id-ID/translation.json'
@@ -27,7 +28,8 @@ i18next.use(initReactI18next).init({
   resources: {
     'en-US': { translation: en },
     'fr-FR': { translation: fr },
-    'id-ID': { translation: id }
+    'id-ID': { translation: id },
+    'el-GR': { translation: el }
   },
   lng: 'en-US',
   fallbackLng: 'en-US',
