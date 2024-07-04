190a329cd4e92f658cc1c843cfe5e86e20a2dc40
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-07-04 17:59:22 +0200

    Fix display of NFTs in tx rows

--- apps/mobile-wallet/src/components/NFTWebView.tsx
+++ apps/mobile-wallet/src/components/NFTWebView.tsx
@@ -17,6 +17,7 @@ along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
 import { memo } from 'react'
+import { View } from 'react-native'
 import WebView from 'react-native-webview'
 
 import { BORDER_RADIUS_SMALL } from '~/style/globalStyle'
@@ -27,28 +28,35 @@ interface NFTWebViewProps {
 }
 
 const NFTWebView = ({ imageUri, size }: NFTWebViewProps) => (
-  <WebView
-    source={{ html: `<img src="${imageUri}" />` }}
-    style={{ width: size, height: size, borderRadius: BORDER_RADIUS_SMALL, backgroundColor: 'transparent' }}
-    injectedJavaScript={
-      "const meta = document.createElement('meta'); meta.setAttribute('content', 'width=width, initial-scale=1, maximum-scale=1, user-scalable=1'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); "
-    }
-    javaScriptEnabled={false}
-    startInLoadingState={true}
-    javaScriptCanOpenWindowsAutomatically={false}
-    allowFileAccess={false}
-    allowFileAccessFromFileURLs={false}
-    allowUniversalAccessFromFileURLs={false}
-    allowsAirPlayForMediaPlayback={false}
-    allowsBackForwardNavigationGestures={false}
-    allowsFullscreenVideo={false}
-    allowsInlineMediaPlayback={false}
-    allowsLinkPreview={false}
-    allowsProtectedMedia={false}
-    scrollEnabled={false}
-    scalesPageToFit={false}
-    onMessage={() => {}}
-  />
+  <View>
+    <WebView
+      source={{ html: `<img src="${imageUri}" />` }}
+      style={{
+        width: size,
+        height: size,
+        borderRadius: BORDER_RADIUS_SMALL,
+        backgroundColor: 'transparent'
+      }}
+      injectedJavaScript={
+        "const meta = document.createElement('meta'); meta.setAttribute('content', 'width=width, initial-scale=1, maximum-scale=1, user-scalable=1'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); "
+      }
+      javaScriptEnabled={false}
+      startInLoadingState={true}
+      javaScriptCanOpenWindowsAutomatically={false}
+      allowFileAccess={false}
+      allowFileAccessFromFileURLs={false}
+      allowUniversalAccessFromFileURLs={false}
+      allowsAirPlayForMediaPlayback={false}
+      allowsBackForwardNavigationGestures={false}
+      allowsFullscreenVideo={false}
+      allowsInlineMediaPlayback={false}
+      allowsLinkPreview={false}
+      allowsProtectedMedia={false}
+      scrollEnabled={false}
+      scalesPageToFit={false}
+      onMessage={() => {}}
+    />
+  </View>
 )
 
 export default memo(NFTWebView)
