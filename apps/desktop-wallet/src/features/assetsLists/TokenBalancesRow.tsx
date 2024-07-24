49f4fe7b7de7acdfb4f6d8c4ad847aa328df01bb
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-07-24 17:23:42 +0200

    Fetch NFTs metadata and data with Tanstack

--- apps/desktop-wallet/src/features/assetsLists/TokenBalancesRow.tsx
+++ apps/desktop-wallet/src/features/assetsLists/TokenBalancesRow.tsx
@@ -189,31 +189,3 @@ const Price = styled.div`
 const NameColumn = styled(Column)`
   margin-right: 50px;
 `
-
-// const PlaceholderText = styled.div`
-//   height: 36px;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-// `
-
-// const NFTList = styled(TableRow)<{ columns?: number }>`
-//   display: grid;
-//   grid-template-columns: repeat(${({ columns }) => columns ?? 5}, minmax(0, 1fr));
-//   grid-auto-flow: initial;
-//   gap: 25px;
-//   padding: 15px;
-//   border-radius: 0 0 12px 12px;
-
-//   > * {
-//     width: 100%;
-//   }
-
-//   ${({ columns }) =>
-//     !columns &&
-//     css`
-//       @media ${deviceBreakPoints.desktop} {
-//         grid-template-columns: repeat(4, minmax(0, 1fr));
-//       }
-//     `}
-// `
