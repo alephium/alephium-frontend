4b4c9995ffec4c1224bd37639dff6428a54b42be
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-09-02 22:28:31 +0200

    Refactor EditWalletNameModal

--- apps/desktop-wallet/src/modals/SettingsModal/EditWalletNameModal.tsx
+++ apps/desktop-wallet/src/modals/SettingsModal/EditWalletNameModal.tsx
@@ -18,14 +18,17 @@ along with the library. If not, see <http://www.gnu.org/licenses/>.
 
 import { getHumanReadableError } from '@alephium/shared'
 import { isEmpty } from 'lodash'
+import { memo } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 
 import { InputFieldsColumn } from '@/components/InputFieldsColumn'
 import Input from '@/components/Inputs/Input'
 import useAnalytics from '@/features/analytics/useAnalytics'
+import { closeModal } from '@/features/modals/modalActions'
+import { ModalBaseProp } from '@/features/modals/modalTypes'
 import { useAppDispatch, useAppSelector } from '@/hooks/redux'
-import CenteredModal, { CenteredModalProps, ModalFooterButton, ModalFooterButtons } from '@/modals/CenteredModal'
+import CenteredModal, { ModalFooterButton, ModalFooterButtons } from '@/modals/CenteredModal'
 import { newWalletNameStored, walletNameStorageFailed } from '@/storage/wallets/walletActions'
 import { walletStorage } from '@/storage/wallets/walletPersistentStorage'
 import { ActiveWallet } from '@/types/wallet'
@@ -35,7 +38,7 @@ type FormData = {
   name: ActiveWallet['name']
 }
 
-const EditWalletNameModal = (props: CenteredModalProps) => {
+const EditWalletNameModal = memo(({ id }: ModalBaseProp) => {
   const { t } = useTranslation()
   const dispatch = useAppDispatch()
   const activeWallet = useAppSelector((state) => state.activeWallet)
@@ -48,13 +51,15 @@ const EditWalletNameModal = (props: CenteredModalProps) => {
   const errors = formState.errors
   const isFormValid = isEmpty(errors)
 
+  const onClose = () => dispatch(closeModal({ id }))
+
   const saveWalletName = (data: FormData) => {
     if (!activeWallet.id) return
 
     try {
       walletStorage.update(activeWallet.id, data)
       dispatch(newWalletNameStored(data.name))
-      props.onClose()
+      onClose()
 
       sendAnalytics({ event: 'Changed wallet name', props: { wallet_name_length: data.name.length } })
     } catch (error) {
@@ -64,7 +69,7 @@ const EditWalletNameModal = (props: CenteredModalProps) => {
   }
 
   return (
-    <CenteredModal title={t('Change wallet name')} narrow {...props}>
+    <CenteredModal title={t('Change wallet name')} narrow id={id}>
       <InputFieldsColumn>
         <Controller
           name="name"
@@ -87,7 +92,7 @@ const EditWalletNameModal = (props: CenteredModalProps) => {
         />
       </InputFieldsColumn>
       <ModalFooterButtons>
-        <ModalFooterButton role="secondary" onClick={props.onClose}>
+        <ModalFooterButton role="secondary" onClick={onClose}>
           {t('Cancel')}
         </ModalFooterButton>
         <ModalFooterButton onClick={handleSubmit(saveWalletName)} disabled={!isFormValid}>
@@ -96,6 +101,6 @@ const EditWalletNameModal = (props: CenteredModalProps) => {
       </ModalFooterButtons>
     </CenteredModal>
   )
-}
+})
 
 export default EditWalletNameModal
