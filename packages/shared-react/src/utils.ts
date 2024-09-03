b031461acdcdc28a57706e9f610e65b3aca734a4
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-09-03 18:14:47 +0200

    Show warning if new modal is not memoized
    Co-authored-by: mvaivre <m.vaivre@gmail.com>

--- packages/shared-react/src/utils.ts
+++ packages/shared-react/src/utils.ts
@@ -16,7 +16,20 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-export * from '@/redux'
-export * from '@/useInitializeClient'
-export * from '@/useInterval'
-export * from '@/utils'
+import { isValidElement, memo, ReactElement } from 'react'
+
+const memoizedFn = memo(() => null)
+
+export const isElementMemoized = <P>(element: ReactElement<P>): boolean => {
+  if (!isValidElement(element)) return false
+
+  const elementType = element.type as unknown as { $$typeof?: symbol }
+
+  return !!elementType.$$typeof && elementType.$$typeof === (memoizedFn as unknown as { $$typeof: symbol }).$$typeof
+}
+
+export const getElementName = <P>(element: ReactElement<P>): string => {
+  const elementType = element.type as React.ComponentType<P>
+
+  return elementType.displayName || elementType.name || 'Component'
+}
