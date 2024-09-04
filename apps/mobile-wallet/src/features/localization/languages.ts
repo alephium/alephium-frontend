fa9f9bc93ebf7fef541e5e5b8591ed9173a6f5db
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-09-04 15:49:20 +0200

    Enable PT in mobile wallet

--- apps/mobile-wallet/src/features/localization/languages.ts
+++ apps/mobile-wallet/src/features/localization/languages.ts
@@ -16,11 +16,12 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-export type Language = 'en-US' | 'fr-FR' | 'el-GR' | 'vi-VN'
+export type Language = 'en-US' | 'fr-FR' | 'el-GR' | 'vi-VN' | 'pt-PT'
 
 export const languageOptions: { label: string; value: Language }[] = [
   { label: 'English', value: 'en-US' },
   { label: 'Français', value: 'fr-FR' },
   { label: 'Ελληνικά', value: 'el-GR' },
-  { label: 'Tiếng Việt', value: 'vi-VN' }
+  { label: 'Tiếng Việt', value: 'vi-VN' },
+  { label: 'Português', value: 'pt-PT' }
 ]
