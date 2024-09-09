f43e4743a3157491f6d40b7c7d9917d707655104
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-09-09 12:54:23 +0200

    Refactor confirm lock time modal

--- apps/desktop-wallet/src/features/send/Transfer/CheckTxModalContent.tsx
+++ apps/desktop-wallet/src/features/send/Transfer/CheckTxModalContent.tsx
@@ -16,29 +16,27 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-import dayjs from 'dayjs'
-import { LockIcon } from 'lucide-react'
-import { useState } from 'react'
-import { Trans, useTranslation } from 'react-i18next'
-import styled from 'styled-components'
+import { useTranslation } from 'react-i18next'
 
 import FooterButton from '@/components/Buttons/FooterButton'
-import InfoBox from '@/components/InfoBox'
+import { openModal } from '@/features/modals/modalActions'
 import CheckAddressesBox from '@/features/send/CheckAddressesBox'
 import CheckAmountsBox from '@/features/send/CheckAmountsBox'
 import CheckFeeLocktimeBox from '@/features/send/CheckFeeLockTimeBox'
 import CheckModalContent from '@/features/send/CheckModalContent'
 import { CheckTxProps, TransferTxData } from '@/features/send/sendTypes'
-import { useAppSelector } from '@/hooks/redux'
-import CenteredModal from '@/modals/CenteredModal'
-import ModalPortal from '@/modals/ModalPortal'
-import { formatDateForDisplay } from '@/utils/misc'
+import { useAppDispatch, useAppSelector } from '@/hooks/redux'
 
 const TransferCheckTxModalContent = ({ data, fees, onSubmit }: CheckTxProps<TransferTxData>) => {
   const { t } = useTranslation()
   const settings = useAppSelector((s) => s.settings)
+  const dispatch = useAppDispatch()
 
-  const [isLockTimeConfirmModalOpen, setIsLockTimeConfirmModalOpen] = useState(false)
+  const handleButtonPress = () => {
+    data.lockTime
+      ? dispatch(openModal({ name: 'ConfirmLockTimeModal', props: { lockTime: data.lockTime, onSubmit } }))
+      : onSubmit()
+  }
 
   return (
     <>
@@ -47,42 +45,9 @@ const TransferCheckTxModalContent = ({ data, fees, onSubmit }: CheckTxProps<Tran
         <CheckAddressesBox fromAddress={data.fromAddress} toAddressHash={data.toAddress} />
         <CheckFeeLocktimeBox fee={fees} lockTime={data.lockTime} />
       </CheckModalContent>
-      <FooterButton onClick={data.lockTime ? () => setIsLockTimeConfirmModalOpen(true) : onSubmit}>
-        {t(settings.passwordRequirement ? 'Confirm' : 'Send')}
-      </FooterButton>
-      <ModalPortal>
-        {isLockTimeConfirmModalOpen && data.lockTime && (
-          <CenteredModal title={t('Confirm lock time')} onClose={() => setIsLockTimeConfirmModalOpen(false)}>
-            <InfoBox importance="accent" Icon={LockIcon}>
-              <Trans
-                t={t}
-                i18nKey="lockTimeConfirmation"
-                values={{
-                  datetime: formatDateForDisplay(data.lockTime),
-                  inTimeFromNow: dayjs(data.lockTime).fromNow()
-                }}
-                components={{
-                  1: <strong />,
-                  3: <FromNow />
-                }}
-              >
-                {
-                  'You chose to lock the assets until <1>{{ datetime }}</1>. That is approximately <3>{{ inTimeFromNow }}</3> from now. Are you sure you want to lock the assets until then?'
-                }
-              </Trans>
-            </InfoBox>
-            <FooterButton onClick={onSubmit} variant="valid">
-              {t('Send locked assets')}
-            </FooterButton>
-          </CenteredModal>
-        )}
-      </ModalPortal>
+      <FooterButton onClick={handleButtonPress}>{t(settings.passwordRequirement ? 'Confirm' : 'Send')}</FooterButton>
     </>
   )
 }
 
 export default TransferCheckTxModalContent
-
-const FromNow = styled.strong`
-  color: ${({ theme }) => theme.global.accent};
-`
