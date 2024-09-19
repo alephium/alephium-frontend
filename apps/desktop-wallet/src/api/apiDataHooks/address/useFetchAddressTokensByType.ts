3b651741f17262dd2b29c16c82b70c77872298ff
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-09-19 17:57:14 +0200

    Refactor useAreAmountsWithinAddressAvailableBalances

--- apps/desktop-wallet/src/api/apiDataHooks/address/useFetchAddressTokensByType.ts
+++ apps/desktop-wallet/src/api/apiDataHooks/address/useFetchAddressTokensByType.ts
@@ -17,34 +17,20 @@ along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
 import { UseFetchAddressProps } from '@/api/apiDataHooks/address/types'
-import useFetchAddressAlphBalances from '@/api/apiDataHooks/address/useFetchAddressAlphBalances'
-import useFetchAddressTokensBalances from '@/api/apiDataHooks/address/useFetchAddressTokensBalances'
+import useFetchAddressBalances from '@/api/apiDataHooks/address/useFetchAddressBalances'
 import useFetchSeparatedTokensByType from '@/api/apiDataHooks/utils/useFetchSeparatedTokensByType'
-import useMergeAllTokensBalances from '@/api/apiDataHooks/utils/useMergeAllTokensBalances'
 
 interface UseFetchAddressTokensByType extends UseFetchAddressProps {
   includeAlph: boolean
 }
 
-const useFetchAddressTokensByType = ({ addressHash, skip, includeAlph }: UseFetchAddressTokensByType) => {
-  const { data: alphBalances, isLoading: isLoadingAlphBalances } = useFetchAddressAlphBalances({
-    addressHash,
-    skip: skip || !includeAlph
-  })
-  const { data: tokensBalances, isLoading: isLoadingTokensBalances } = useFetchAddressTokensBalances({
-    addressHash,
-    skip
-  })
-  const allTokensBalances = useMergeAllTokensBalances({
-    includeAlph,
-    alphBalances,
-    tokensBalances: tokensBalances?.balances
-  })
+const useFetchAddressTokensByType = (props: UseFetchAddressTokensByType) => {
+  const { data: allTokensBalances, isLoading: isLoadingBalances } = useFetchAddressBalances(props)
   const { data, isLoading } = useFetchSeparatedTokensByType(allTokensBalances)
 
   return {
     data,
-    isLoading: isLoading || isLoadingTokensBalances || isLoadingAlphBalances
+    isLoading: isLoading || isLoadingBalances
   }
 }
 
