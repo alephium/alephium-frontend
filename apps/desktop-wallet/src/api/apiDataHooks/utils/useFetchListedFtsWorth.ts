cd5319513693a12379da721e33d5ca5d85c30bc2
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-09-21 19:20:08 +0200

    Rework charts to remove old hooks

---
+++ apps/desktop-wallet/src/api/apiDataHooks/utils/useFetchListedFtsWorth.ts
@@ -0,0 +1,50 @@
+/*
+Copyright 2018 - 2024 The Alephium Authors
+This file is part of the alephium project.
+
+The library is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the library. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import { calculateAmountWorth } from '@alephium/shared'
+import { useMemo } from 'react'
+
+import useFetchTokenPrices from '@/api/apiDataHooks/useFetchTokenPrices'
+import { DisplayBalances, ListedFT } from '@/types/tokens'
+
+interface UseListedFtsWorthProps {
+  listedFts: (ListedFT & DisplayBalances)[]
+}
+
+const useFetchListedFtsWorth = ({ listedFts }: UseListedFtsWorthProps) => {
+  const { data: tokenPrices, isLoading: isLoadingTokenPrices } = useFetchTokenPrices()
+
+  const worth = useMemo(
+    () =>
+      listedFts.reduce((totalWorth, token) => {
+        const tokenPrice = tokenPrices?.find(({ symbol }) => symbol === token.symbol)?.price
+        const tokenWorth =
+          tokenPrice !== undefined ? calculateAmountWorth(token.totalBalance, tokenPrice, token.decimals) : 0
+
+        return totalWorth + tokenWorth
+      }, 0),
+    [tokenPrices, listedFts]
+  )
+
+  return {
+    data: worth,
+    isLoading: isLoadingTokenPrices
+  }
+}
+
+export default useFetchListedFtsWorth
