2e07f3263c01b7e115d203420e55819452ec6798
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-10-03 15:28:46 +0200

    Display warning when > 80 addresses

---
+++ apps/desktop-wallet/src/api/apiDataHooks/wallet/useFetchWalletTransactionsLimitted.tsx
@@ -0,0 +1,47 @@
+/*
+Copyright 2018 - 2024 The Alephium Authors
+This file is part of the alephium project.
+
+The library is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the library. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import { useQuery } from '@tanstack/react-query'
+
+import useLimitedWalletAddresses from '@/api/apiDataHooks/utils/useLimitedWalletAddresses'
+import { useFetchWalletLastTransaction } from '@/api/apiDataHooks/wallet/useFetchWalletLastTransactions'
+import { walletLatestTransactionsQuery } from '@/api/queries/transactionQueries'
+import { useAppSelector } from '@/hooks/redux'
+
+const useFetchWalletTransactionsLimitted = () => {
+  const networkId = useAppSelector((s) => s.network.settings.networkId)
+  const { addressHashes, isLimited } = useLimitedWalletAddresses()
+
+  const { data } = useFetchWalletLastTransaction()
+  const { data: confirmedTxs, isLoading } = useQuery(
+    walletLatestTransactionsQuery({
+      addressHashes,
+      latestTxHash: data?.latestTx?.hash,
+      previousTxHash: data?.previousTx?.hash,
+      networkId
+    })
+  )
+
+  return {
+    data: confirmedTxs,
+    isLoading,
+    isDataComplete: !isLimited
+  }
+}
+
+export default useFetchWalletTransactionsLimitted
