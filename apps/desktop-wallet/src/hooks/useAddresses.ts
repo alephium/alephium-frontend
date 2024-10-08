d89026d4e219b97793807bacefc0778aa17c732c
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-10-08 12:07:31 +0200

    Merge all address hashes hooks in one file

---
+++ apps/desktop-wallet/src/hooks/useAddresses.ts
@@ -0,0 +1,80 @@
+/*
+Copyright 2018 - 2024 The Alephium Authors
+This file is part of the alephium project.
+
+The library is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the library. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import { ADDRESSES_QUERY_LIMIT, AddressHash } from '@alephium/shared'
+import { orderBy } from 'lodash'
+import { useMemo } from 'react'
+
+import { SkipProp } from '@/api/apiDataHooks/apiDataHooksTypes'
+import useFetchLatestTransactionOfEachAddress from '@/api/apiDataHooks/wallet/useFetchLatestTransactionOfEachAddress'
+import { useAppSelector } from '@/hooks/redux'
+import { selectAllAddressHashes, selectDefaultAddress } from '@/storage/addresses/addressesSelectors'
+
+export const useUnsortedAddressesHashes = (): AddressHash[] => useAppSelector(selectAllAddressHashes)
+
+export const useFetchSortedAddressesHashes = (props?: SkipProp) => {
+  const allAddressHashes = useAppSelector(selectAllAddressHashes)
+  const { data: sortedAddresses, isLoading } = useFetchSortedAddressesHashesWithLatestTx(props)
+
+  const sortedAddressHashes = useMemo(() => sortedAddresses.map(({ addressHash }) => addressHash), [sortedAddresses])
+
+  return {
+    data: !isLoading && !props?.skip ? sortedAddressHashes : allAddressHashes,
+    isLoading
+  }
+}
+
+export const useFetchSortedAddressesHashesWithLatestTx = (props?: SkipProp) => {
+  const { hash: defaultAddressHash } = useAppSelector(selectDefaultAddress)
+  const { data: latestTxs, isLoading: isLoadingLatestTxs } = useFetchLatestTransactionOfEachAddress(props)
+
+  return {
+    data: useMemo(
+      () =>
+        orderBy(
+          latestTxs,
+          ({ addressHash, latestTx }) => (addressHash === defaultAddressHash ? undefined : latestTx?.timestamp ?? 0),
+          'desc'
+        ),
+      [latestTxs, defaultAddressHash]
+    ),
+    isLoading: isLoadingLatestTxs
+  }
+}
+
+export const useCappedAddressesHashes = () => {
+  const allAddressHashes = useAppSelector(selectAllAddressHashes)
+
+  const exceedsAddressLimit = allAddressHashes.length > ADDRESSES_QUERY_LIMIT
+  const { data: allAddressHashesSorted, isLoading: isLoadingSortedAddresses } = useFetchSortedAddressesHashes({
+    skip: !exceedsAddressLimit
+  })
+
+  return {
+    isCapped: exceedsAddressLimit,
+    addressHashes: useMemo(
+      () =>
+        exceedsAddressLimit
+          ? !isLoadingSortedAddresses
+            ? allAddressHashesSorted.slice(0, ADDRESSES_QUERY_LIMIT)
+            : []
+          : allAddressHashes,
+      [allAddressHashes, allAddressHashesSorted, exceedsAddressLimit, isLoadingSortedAddresses]
+    )
+  }
+}
