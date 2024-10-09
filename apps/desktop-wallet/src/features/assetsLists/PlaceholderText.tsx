030575b2334d20cbd1e550d36fc8e6c77255ff34
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-10-09 10:14:04 +0200

    Make placeholder text color lighter

--- apps/desktop-wallet/src/features/assetsLists/PlaceholderText.tsx
+++ apps/desktop-wallet/src/features/assetsLists/PlaceholderText.tsx
@@ -24,6 +24,7 @@ const PlaceholderText = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  color: ${({ theme }) => theme.font.tertiary};
 `
 
 export default PlaceholderText
