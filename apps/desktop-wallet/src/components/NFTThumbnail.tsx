852be2e55046a0f869bdc0a5f51b12aed70e37e0
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-10-11 18:02:34 +0200

    Hide placeholder when displaying error

--- apps/desktop-wallet/src/components/NFTThumbnail.tsx
+++ apps/desktop-wallet/src/components/NFTThumbnail.tsx
@@ -30,15 +30,18 @@ interface NFTThumbnailProps {
   nftId: NFT['id']
   size?: string
   onClick?: () => void
+  hideIfError?: boolean
   className?: string
 }
 
-const NFTThumbnail = ({ nftId, size = '100', ...props }: NFTThumbnailProps) => {
+const NFTThumbnail = ({ nftId, size = '100', hideIfError = false, ...props }: NFTThumbnailProps) => {
   const { data: nft, isLoading, error: fetchNftError } = useFetchNft({ id: nftId })
 
   const [error, setError] = useState(false)
 
-  if (isLoading) return <SkeletonLoader height={size === '100%' ? '300px' : size} />
+  if (isLoading) return <SkeletonLoader height={size === '100%' ? '130px' : size} />
+
+  if (hideIfError && fetchNftError) return null
 
   if (fetchNftError)
     return (
