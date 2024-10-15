96528b51e9fc6901c7a8f4816710e1fa128a7552
mvaivre <m.vaivre@gmail.com>
2024-10-15 10:24:36 +0200

    Add padding bottom to BottomModal on Android

--- apps/mobile-wallet/src/features/modals/useBottomModalState.tsx
+++ apps/mobile-wallet/src/features/modals/useBottomModalState.tsx
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 import { useCallback, useEffect, useState } from 'react'
-import { NativeScrollEvent, NativeSyntheticEvent, useWindowDimensions } from 'react-native'
+import { NativeScrollEvent, NativeSyntheticEvent, Platform, useWindowDimensions } from 'react-native'
 import { Gesture } from 'react-native-gesture-handler'
 import { interpolate, runOnJS, runOnUI, useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
@@ -40,6 +40,8 @@ export interface ContentScrollHandlers {
   onScrollBeginDrag: () => void
   onScrollEndDrag: () => void
 }
+
+const isIos = Platform.OS === 'ios'
 
 const springConfig = {
   damping: 50,
@@ -127,7 +129,7 @@ export const useBottomModalState = ({
             ? customMinHeight
             : shouldMaximizeOnOpen.value
               ? maxHeight
-              : contentHeight.value + customNavHeight + insets.bottom
+              : contentHeight.value + customNavHeight + (isIos ? insets.bottom : insets.bottom + 18)
 
           shouldMaximizeOnOpen.value ? handleMaximize() : handleMinimize()
 
