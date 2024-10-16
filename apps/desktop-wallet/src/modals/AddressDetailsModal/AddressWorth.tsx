57846c2862eebc7c23a8aa16d2c59b0b8df7ee22
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-10-16 10:22:31 +0200

    Fix display of address worth in address details modal

--- apps/desktop-wallet/src/modals/AddressDetailsModal/AddressWorth.tsx
+++ apps/desktop-wallet/src/modals/AddressDetailsModal/AddressWorth.tsx
@@ -18,17 +18,18 @@ along with the library. If not, see <http://www.gnu.org/licenses/>.
 
 import { memo } from 'react'
 
-import useFetchWalletWorth from '@/api/apiDataHooks/wallet/useFetchWalletWorth'
+import useFetchAddressWorth from '@/api/apiDataHooks/address/useFetchAddressWorth'
 import WorthOverview from '@/components/WorthOverview'
 
-interface WalletWorthProps {
+interface AddressWorthProps {
+  addressHash: string
   overrideWorth?: number
 }
 
-const WalletWorth = memo((props: WalletWorthProps) => {
-  const { data: worth, isLoading } = useFetchWalletWorth()
+const AddressWorth = memo(({ addressHash, ...props }: AddressWorthProps) => {
+  const { data: worth, isLoading } = useFetchAddressWorth(addressHash)
 
   return <WorthOverview worth={worth} isLoading={isLoading} {...props} />
 })
 
-export default WalletWorth
+export default AddressWorth
