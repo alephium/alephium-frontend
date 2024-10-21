5e2db37909d961cf259830e6d93a0f027f468309
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-10-21 12:28:16 +0200

    Fix auto-lock

--- apps/desktop-wallet/src/hooks/useAutoLock.ts
+++ apps/desktop-wallet/src/hooks/useAutoLock.ts
@@ -17,13 +17,10 @@ along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
 import { ONE_MINUTE_MS } from '@alephium/shared'
-import { throttle } from 'lodash'
-import { useEffect } from 'react'
+import { useIdleTimer } from 'react-idle-timer'
 
 import { useAppSelector } from '@/hooks/redux'
 import useWalletLock from '@/hooks/useWalletLock'
-
-// Inspired by useIdle: https://github.com/uidotdev/usehooks/blob/main/index.js
 
 const useAutoLock = () => {
   const walletLockTimeInMinutes = useAppSelector((s) => s.settings.walletLockTimeInMinutes)
@@ -31,47 +28,12 @@ const useAutoLock = () => {
 
   const { lockWallet, isWalletUnlocked } = useWalletLock()
 
-  useEffect(() => {
-    if (!isWalletUnlocked || lockAfterMs === 0) return
-
-    let timeoutId: number | undefined
-
-    const handleTimeout = () => {
-      lockWallet('Auto lock')
-    }
-
-    const handleEvent = throttle(() => {
-      window.clearTimeout(timeoutId)
-      timeoutId = window.setTimeout(handleTimeout, lockAfterMs)
-    }, 500)
-
-    const handleVisibilityChange = () => {
-      if (!document.hidden) {
-        handleEvent()
-      }
-    }
-
-    timeoutId = window.setTimeout(handleTimeout, lockAfterMs)
-
-    window.addEventListener('mousemove', handleEvent)
-    window.addEventListener('mousedown', handleEvent)
-    window.addEventListener('resize', handleEvent)
-    window.addEventListener('keydown', handleEvent)
-    window.addEventListener('touchstart', handleEvent)
-    window.addEventListener('wheel', handleEvent)
-    document.addEventListener('visibilitychange', handleVisibilityChange)
-
-    return () => {
-      window.removeEventListener('mousemove', handleEvent)
-      window.removeEventListener('mousedown', handleEvent)
-      window.removeEventListener('resize', handleEvent)
-      window.removeEventListener('keydown', handleEvent)
-      window.removeEventListener('touchstart', handleEvent)
-      window.removeEventListener('wheel', handleEvent)
-      document.removeEventListener('visibilitychange', handleVisibilityChange)
-      window.clearTimeout(timeoutId)
-    }
-  }, [isWalletUnlocked, lockAfterMs, lockWallet])
+  useIdleTimer({
+    onIdle: () => lockWallet('Auto lock'),
+    timeout: lockAfterMs,
+    throttle: 500,
+    disabled: !isWalletUnlocked
+  })
 }
 
 export default useAutoLock
