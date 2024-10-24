5fda70da349ed8ae9784239cfa9bceb70dc7ef0b
mvaivre <m.vaivre@gmail.com>
2024-10-24 18:05:02 +0200

    Improve NFT display support (inspired by explorer)

--- apps/desktop-wallet/src/features/assetsLists/NFTsGrid.tsx
+++ apps/desktop-wallet/src/features/assetsLists/NFTsGrid.tsx
@@ -107,7 +107,7 @@ const Grid = styled(TableRow)<{ columns: number }>`
   display: grid;
   grid-template-columns: repeat(${({ columns }) => columns}, minmax(0, 1fr));
   grid-auto-flow: initial;
-  gap: 25px;
+  gap: 15px;
   padding: 15px;
   border-radius: 0 0 12px 12px;
 
