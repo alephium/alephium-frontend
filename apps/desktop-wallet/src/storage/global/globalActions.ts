106c96957feb72ea3d39e2245d5551ae54e41eb9
mvaivre <m.vaivre@gmail.com>
2024-11-05 09:19:35 +0100

    Implement devnet faucet on DW

--- apps/desktop-wallet/src/storage/global/globalActions.ts
+++ apps/desktop-wallet/src/storage/global/globalActions.ts
@@ -16,11 +16,13 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { AddressHash, exponentialBackoffFetchRetry } from '@alephium/shared'
+import { AddressHash, exponentialBackoffFetchRetry, throttledClient } from '@alephium/shared'
+import { transactionSign } from '@alephium/web3'
 import { createAction, createAsyncThunk } from '@reduxjs/toolkit'
 
 import { ThemeType } from '@/features/theme/themeTypes'
 import i18n from '@/i18n'
+import { RootState } from '@/storage/store'
 import { OptionalMessage, SnackbarMessage } from '@/types/snackbar'
 import { SentTransaction } from '@/types/transactions'
 
@@ -60,7 +62,46 @@ export const appDataClearFailed = createAction('app/appDataClearFailed')
 
 export const receiveFaucetTokens = createAsyncThunk<SentTransaction, AddressHash, { rejectValue: SnackbarMessage }>(
   'assets/receiveFaucetTokens',
-  async (destinationAddress: AddressHash, { rejectWithValue, fulfillWithValue }) => {
+  async (destinationAddress: AddressHash, { getState, rejectWithValue, fulfillWithValue }) => {
+    const state = getState() as RootState
+    const currentNetwork = state.network
+
+    const txBoilerplate: Omit<SentTransaction, 'hash'> = {
+      fromAddress: 'Faucet',
+      toAddress: destinationAddress,
+      amount: undefined,
+      timestamp: new Date().getTime(),
+      status: 'sent',
+      type: 'faucet'
+    }
+
+    if (currentNetwork.name === 'devnet') {
+      throttledClient.init(currentNetwork.settings.nodeHost, currentNetwork.settings.explorerApiHost)
+
+      const faucetPublicKey = '0381818e63bd9e35a5489b52a430accefc608fd60aa2c7c0d1b393b5239aedf6b0'
+      const faucetPrivateKey = 'a642942e67258589cd2b1822c631506632db5a12aabcf413604e785300d762a5'
+
+      const builtTx = await throttledClient.node.transactions.postTransactionsBuild({
+        fromPublicKey: faucetPublicKey,
+        destinations: [
+          {
+            address: destinationAddress,
+            attoAlphAmount: BigInt(1e21).toString()
+          }
+        ]
+      })
+
+      const txRes = await throttledClient.node.transactions.postTransactionsSubmit({
+        unsignedTx: builtTx.unsignedTx,
+        signature: transactionSign(builtTx.txId, faucetPrivateKey)
+      })
+
+      return fulfillWithValue({
+        ...txBoilerplate,
+        hash: txRes.txId
+      })
+    }
+
     const response = await exponentialBackoffFetchRetry('https://faucet.testnet.alephium.org/send', {
       method: 'POST',
       body: destinationAddress
@@ -80,16 +121,9 @@ export const receiveFaucetTokens = createAsyncThunk<SentTransaction, AddressHash
 
     const hash = responseURL.match(/\/([a-fA-F0-9]+)$/)?.[1] || ''
 
-    const pendingTransaction: SentTransaction = {
-      hash: hash,
-      fromAddress: 'Faucet',
-      toAddress: destinationAddress,
-      amount: undefined,
-      timestamp: new Date().getTime(),
-      status: 'sent',
-      type: 'faucet'
-    }
-
-    return fulfillWithValue(pendingTransaction)
+    return fulfillWithValue({
+      ...txBoilerplate,
+      hash: hash
+    })
   }
 )
