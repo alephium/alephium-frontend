a9d458656718c62d858dc54e58f34feba858757a
mvaivre <m.vaivre@gmail.com>
2024-11-11 18:02:59 +0100

    Update default badge

--- apps/mobile-wallet/src/images/DefaultAddressBadge.tsx
+++ apps/mobile-wallet/src/images/DefaultAddressBadge.tsx
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { Star } from 'lucide-react-native'
+import { Canvas, FitBox, Path, rect } from '@shopify/react-native-skia'
 import { StyleProp, ViewStyle } from 'react-native'
 import styled from 'styled-components/native'
 
@@ -26,21 +26,31 @@ interface DefaultAddressBadgeProps {
   style?: StyleProp<ViewStyle>
 }
 
-const DefaultAddressBadge = ({ size = 24, ...props }: DefaultAddressBadgeProps) => (
-  <DefaultAddressBadgeStyled size={size} {...props}>
-    <StarStyled size={size - 2} color="white" style={{ fill: 'white' }} />
+const DefaultAddressBadge = ({ size = 24, color = 'blue', ...props }: DefaultAddressBadgeProps) => (
+  <DefaultAddressBadgeStyled size={size} color={color} {...props}>
+    <StarShape size={size - 4} color="white" />
   </DefaultAddressBadgeStyled>
 )
+
+const StarShape = ({ size, color = 'white' }: { size: number; color: string }) => {
+  const path = 'M12 1L15.5 8L24 9L17.5 14L19 22L12 18L5 22L6.5 14L0 9L8.5 8L12 1Z'
+
+  return (
+    <Canvas style={{ width: size, height: size }}>
+      <FitBox src={rect(0, 0, 24, 24)} dst={rect(0, 0, size, size)}>
+        <Path path={path} color={color} style="fill" />
+      </FitBox>
+    </Canvas>
+  )
+}
 
 export default DefaultAddressBadge
 
 const DefaultAddressBadgeStyled = styled.View<DefaultAddressBadgeProps>`
   justify-content: center;
   align-items: center;
-  border-radius: ${({ size }) => size}px;
+  border-radius: ${({ size }) => size! / 2}px;
   background-color: ${({ color }) => color};
   height: ${({ size }) => size}px;
   width: ${({ size }) => size}px;
 `
-
-const StarStyled = styled(Star)``
