e849c66b3fc1d7aa6501d37c57cf1e9dee421c43
mvaivre <m.vaivre@gmail.com>
2024-11-21 18:41:31 +0100

    Simplify BuyModal

--- apps/desktop-wallet/src/features/buy/BuyModal.tsx
+++ apps/desktop-wallet/src/features/buy/BuyModal.tsx
@@ -17,12 +17,12 @@ along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
 import { AddressHash } from '@alephium/shared'
-import { memo, useState } from 'react'
+import { memo } from 'react'
 import { Trans, useTranslation } from 'react-i18next'
 import styled, { useTheme } from 'styled-components'
 
 import ActionLink from '@/components/ActionLink'
-import Button from '@/components/Button'
+import FooterButton from '@/components/Buttons/FooterButton'
 import { ModalBaseProp } from '@/features/modals/modalTypes'
 import CenteredModal from '@/modals/CenteredModal'
 import { electron, openInWebBrowser } from '@/utils/misc'
@@ -34,7 +34,6 @@ export interface BuyModalProps {
 const BuyModal = memo(({ id, addressHash }: ModalBaseProp & BuyModalProps) => {
   const { t } = useTranslation()
   const theme = useTheme()
-  const [isDisclaimerAccepted, setIsDisclaimerAccepted] = useState(false)
 
   const banxaURL =
     'https://alephium.banxa.com/' +
@@ -46,65 +45,32 @@ const BuyModal = memo(({ id, addressHash }: ModalBaseProp & BuyModalProps) => {
     `&secondaryColor=${theme.global.complementary.slice(1)}`
 
   const handleAcceptDisclaimer = () => {
-    setIsDisclaimerAccepted(true)
     electron?.app.openOnRampServiceWindow({ url: banxaURL, targetLocation: 'https://alephium.org' })
   }
 
   return (
-    <CenteredModal id={id} title={t('Buy')} fullScreen dynamicContent noPadding>
-      <Content>
-        <IFrameStyled title={t('Buy')} src={banxaURL} />
-        {!isDisclaimerAccepted && (
-          <DisclaimerOverlay>
-            <h1 style={{ margin: 0 }}>{t('Disclaimer')}</h1>
-            <span>
-              <Trans t={t} i18nKey="banxaDisclaimer">
-                You are about to access 3rd party services provided by
-                <ActionLinkStyled onClick={() => openInWebBrowser('https://www.banxa.com')}></ActionLinkStyled> through
-                an in-app browser. Alephium does not control Banxa’s services. Banxa’s terms and conditions will apply,
-                so please read and understand them before proceeding.
-              </Trans>
-            </span>
-            <Button onClick={handleAcceptDisclaimer} role="primary" tall>
-              {t('Continue')}
-            </Button>
-          </DisclaimerOverlay>
-        )}
-      </Content>
+    <CenteredModal id={id} title={t('Disclaimer')}>
+      <DisclaimerText>
+        <Trans t={t} i18nKey="banxaDisclaimer">
+          You are about to access 3rd party services provided by
+          <ActionLinkStyled onClick={() => openInWebBrowser('https://www.banxa.com')}></ActionLinkStyled> through an
+          in-app browser. Alephium does not control Banxa’s services. Banxa’s terms and conditions will apply, so please
+          read and understand them before proceeding.
+        </Trans>
+      </DisclaimerText>
+      <FooterButton onClick={handleAcceptDisclaimer} role="primary" tall>
+        {t('Continue')}
+      </FooterButton>
     </CenteredModal>
   )
 })
 
 export default BuyModal
 
-const IFrameStyled = styled.iframe`
-  width: 100%;
-  height: 100%;
-  border: 0;
-`
-
-const Content = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100%;
-`
-
-const DisclaimerOverlay = styled.div`
-  position: absolute;
-  top: 0;
-  right: 0;
-  left: 0;
-  bottom: 0;
-  background-color: ${({ theme }) => theme.bg.background2};
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 30px;
-  margin: auto;
-  padding: 10% 30%;
-`
-
 const ActionLinkStyled = styled(ActionLink)`
   display: inline;
+`
+
+const DisclaimerText = styled.span`
+  font-size: 14px;
 `
