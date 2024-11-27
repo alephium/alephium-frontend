ab112bc09932c40f8b515107d4183188b4c642f0
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-11-27 14:37:22 +0100

    Validate IPC sender
    See: https://www.electronjs.org/docs/latest/tutorial/security#17-validate-the-sender-of-all-ipc-messages

--- apps/desktop-wallet/public/electron.js
+++ apps/desktop-wallet/public/electron.js
@@ -256,18 +256,26 @@ app.on('ready', async function () {
     await installExtension(REDUX_DEVTOOLS)
   }
 
-  ipcMain.handle('theme:setNativeTheme', (_, theme) => (nativeTheme.themeSource = theme))
+  ipcMain.handle('theme:setNativeTheme', ({ senderFrame }, theme) => {
+    if (!isIpcSenderValid(senderFrame)) return null
+
+    nativeTheme.themeSource = theme
+  })
 
   // nativeTheme must be reassigned like this because its properties are all computed, so
   // they can't be serialized to be passed over channels.
-  ipcMain.handle('theme:getNativeTheme', ({ sender }) =>
+  ipcMain.handle('theme:getNativeTheme', ({ sender, senderFrame }) => {
+    if (!isIpcSenderValid(senderFrame)) return null
+
     sender.send('theme:getNativeTheme', {
       shouldUseDarkColors: nativeTheme.shouldUseDarkColors,
       themeSource: nativeTheme.themeSource
     })
-  )
+  })
 
-  ipcMain.handle('updater:checkForUpdates', async () => {
+  ipcMain.handle('updater:checkForUpdates', async ({ senderFrame }) => {
+    if (!isIpcSenderValid(senderFrame)) return null
+
     try {
       const result = await autoUpdater.checkForUpdates()
 
@@ -277,11 +285,21 @@ app.on('ready', async function () {
     }
   })
 
-  ipcMain.handle('updater:startUpdateDownload', () => autoUpdater.downloadUpdate())
+  ipcMain.handle('updater:startUpdateDownload', ({ senderFrame }) => {
+    if (!isIpcSenderValid(senderFrame)) return null
 
-  ipcMain.handle('updater:quitAndInstallUpdate', () => autoUpdater.quitAndInstall())
+    autoUpdater.downloadUpdate()
+  })
 
-  ipcMain.handle('app:hide', () => {
+  ipcMain.handle('updater:quitAndInstallUpdate', ({ senderFrame }) => {
+    if (!isIpcSenderValid(senderFrame)) return null
+
+    autoUpdater.quitAndInstall()
+  })
+
+  ipcMain.handle('app:hide', ({ senderFrame }) => {
+    if (!isIpcSenderValid(senderFrame)) return null
+
     if (isWindows) {
       mainWindow.blur()
     } else {
@@ -289,7 +307,9 @@ app.on('ready', async function () {
     }
   })
 
-  ipcMain.handle('app:show', () => {
+  ipcMain.handle('app:show', ({ senderFrame }) => {
+    if (!isIpcSenderValid(senderFrame)) return null
+
     if (isWindows) {
       mainWindow.minimize()
       mainWindow.restore()
@@ -298,17 +318,23 @@ app.on('ready', async function () {
     }
   })
 
-  ipcMain.handle('app:getSystemLanguage', () => {
+  ipcMain.handle('app:getSystemLanguage', ({ senderFrame }) => {
+    if (!isIpcSenderValid(senderFrame)) return null
+
     const preferedLanguages = app.getPreferredSystemLanguages()
 
     if (preferedLanguages.length > 0) return preferedLanguages[0]
   })
 
-  ipcMain.handle('app:getSystemRegion', () => {
+  ipcMain.handle('app:getSystemRegion', ({ senderFrame }) => {
+    if (!isIpcSenderValid(senderFrame)) return null
+
     return app.getSystemLocale()
   })
 
-  ipcMain.handle('app:setProxySettings', async (_, proxySettings) => {
+  ipcMain.handle('app:setProxySettings', async ({ senderFrame }, proxySettings) => {
+    if (!isIpcSenderValid(senderFrame)) return null
+
     const { address, port } = proxySettings
     const proxyRules = !address && !port ? undefined : `socks5://${address}:${port}`
 
@@ -319,14 +345,22 @@ app.on('ready', async function () {
     }
   })
 
-  ipcMain.handle('app:restart', () => {
+  ipcMain.handle('app:restart', ({ senderFrame }) => {
+    if (!isIpcSenderValid(senderFrame)) return null
+
     app.relaunch()
     app.exit()
   })
 
-  ipcMain.handle('wc:getDeepLinkUri', () => deepLinkUri)
+  ipcMain.handle('wc:getDeepLinkUri', ({ senderFrame }) => {
+    if (!isIpcSenderValid(senderFrame)) return null
 
-  ipcMain.handle('wc:resetDeepLinkUri', () => {
+    return deepLinkUri
+  })
+
+  ipcMain.handle('wc:resetDeepLinkUri', ({ senderFrame }) => {
+    if (!isIpcSenderValid(senderFrame)) return null
+
     deepLinkUri = null
   })
 
@@ -362,3 +396,8 @@ ipcMain.on('shell:open', () => {
   const pagePath = path.join('file://', pageDirectory, 'index.html')
   shell.openExternal(pagePath)
 })
+
+// See: https://www.electronjs.org/docs/latest/tutorial/security#17-validate-the-sender-of-all-ipc-messages
+const isIpcSenderValid = (frame) => {
+  return new URL(frame.url).host === 'localhost:3000'
+}
