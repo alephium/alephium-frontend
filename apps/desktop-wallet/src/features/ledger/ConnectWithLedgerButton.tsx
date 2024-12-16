763854f4af7dce983e5f8c22f79c316ccce0bfd2
Ilias Trichopoulos <hlias.nop@gmail.com>
2024-12-16 10:38:32 +0100

    Update styles of ledger button

--- apps/desktop-wallet/src/features/ledger/ConnectWithLedgerButton.tsx
+++ apps/desktop-wallet/src/features/ledger/ConnectWithLedgerButton.tsx
@@ -20,6 +20,7 @@ import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
 
 import Button from '@/components/Button'
+import { ReactComponent as LedgerLogo } from '@/images/ledger.svg'
 
 const ConnectWithLedgerButton = () => {
   const { t } = useTranslation()
@@ -29,7 +30,12 @@ const ConnectWithLedgerButton = () => {
     navigate('/ledger')
   }
 
-  return <Button onClick={handleLedgerConnectClick}>{t('Connect with Ledger')}</Button>
+  return (
+    <Button onClick={handleLedgerConnectClick} role="secondary" variant="faded" short style={{ width: '80%' }}>
+      <LedgerLogo style={{ width: '15px', marginRight: '10px' }} />
+      {t('Connect with Ledger')}
+    </Button>
+  )
 }
 
 export default ConnectWithLedgerButton
