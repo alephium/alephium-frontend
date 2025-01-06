66849a9568df9547a480b7ce9a9c94e516757ab9
mvaivre <m.vaivre@gmail.com>
2025-01-06 23:19:32 +0100

    Start to design dApp cards, show images, and open web browser

--- apps/mobile-wallet/src/features/ecosystem/DAppCard.tsx
+++ apps/mobile-wallet/src/features/ecosystem/DAppCard.tsx
@@ -1,18 +1,33 @@
+import { Image } from 'expo-image'
+import * as WebBrowser from 'expo-web-browser'
 import styled from 'styled-components/native'
 
 import AppText from '~/components/AppText'
 import { DApp } from '~/features/ecosystem/ecosystemTypes'
 
-const DAppCard = ({ name }: DApp) => (
-  <CardContainer>
-    <AppText>{name}</AppText>
-  </CardContainer>
-)
+const DAppCard = ({ name, media, links }: DApp) => {
+  const handleCardPress = async () => {
+    await WebBrowser.openBrowserAsync(links.website)
+  }
+
+  return (
+    <CardContainer onPress={handleCardPress}>
+      <CardImage source={{ uri: media.bannerUrl }} contentFit="cover" />
+      <AppText>{name}</AppText>
+    </CardContainer>
+  )
+}
 
 export default DAppCard
 
-const CardContainer = styled.View`
+const CardContainer = styled.Pressable`
   align-items: center;
-  background-color: ${({ theme }) => theme.bg.primary};
+  background-color: ${({ theme }) => theme.bg.back1};
   border-radius: 9px;
+  padding: 12px;
+`
+
+const CardImage = styled(Image)`
+  height: 140px;
+  width: 100%;
 `
