0916a625363ed55303e7150b00bfe5ffe8644024
mvaivre <m.vaivre@gmail.com>
2025-01-14 21:09:54 +0100

    BottomModal: remove content scroll handlers We may use react-native-bottom-sheet in the future

--- apps/mobile-wallet/src/features/modals/BottomModalFlashList.tsx
+++ apps/mobile-wallet/src/features/modals/BottomModalFlashList.tsx
@@ -2,15 +2,12 @@ import { ContentStyle } from '@shopify/flash-list'
 import { ReactNode } from 'react'
 
 import BottomModalBase, { BottomModalBaseProps } from '~/features/modals/BottomModalBase'
-import { ContentScrollHandlers, useBottomModalState } from '~/features/modals/useBottomModalState'
+import { useBottomModalState } from '~/features/modals/useBottomModalState'
 import { DEFAULT_MARGIN } from '~/style/globalStyle'
 
 export interface BottomModalFlashListProps extends Omit<BottomModalBaseProps, 'children'> {
   flashListRender: (flashListProps: {
     contentContainerStyle: ContentStyle
-    onScroll: ContentScrollHandlers['onScroll']
-    onScrollBeginDrag: ContentScrollHandlers['onScrollBeginDrag']
-    onScrollEndDrag: ContentScrollHandlers['onScrollEndDrag']
     onContentSizeChange: (w: number, h: number) => void
   }) => ReactNode
 }
@@ -37,7 +34,6 @@ const BottomModalFlashList = ({
   return (
     <BottomModalBase title={title} modalId={modalId} navHeight={navHeight} titleAlign={titleAlign} {...modalState}>
       {flashListRender({
-        ...modalState.contentScrollHandlers,
         onContentSizeChange: modalState.handleContentSizeChange,
         contentContainerStyle: {
           padding: noPadding ? 0 : DEFAULT_MARGIN
