1eeb6023b9e2569566c14a3ad010ae1d2e27e5a0
mvaivre <m.vaivre@gmail.com>
2025-01-14 19:25:04 +0100

    Slightly adapt dashboard card's header margin

--- apps/mobile-wallet/src/screens/Dashboard/DashboardSecondaryButtons.tsx
+++ apps/mobile-wallet/src/screens/Dashboard/DashboardSecondaryButtons.tsx
@@ -25,11 +25,12 @@ const DashboardSecondaryButtons = () => {
     })
 
   const areNoButtonsVisible = !isWalletConnectEnabled && isMnemonicBackedUp && networkStatus !== 'offline'
+  const onlyLeftButtonVisible = isWalletConnectEnabled && isMnemonicBackedUp && networkStatus !== 'offline'
 
   if (areNoButtonsVisible) return null
 
   return (
-    <DashboardSecondaryButtonsStyled style={{ height: areNoButtonsVisible ? 20 : 30 }}>
+    <DashboardSecondaryButtonsStyled style={{ height: onlyLeftButtonVisible ? 15 : 30 }}>
       <Buttons>
         {isWalletConnectEnabled && <WalletConnectButton />}
         {networkStatus === 'offline' && (
