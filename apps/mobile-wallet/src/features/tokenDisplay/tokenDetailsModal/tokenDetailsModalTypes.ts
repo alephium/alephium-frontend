6c5aa0680289d559f1a2ceafb583543a822b7a7a
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-01-14 17:19:50 +0800

    Create token addresses modal

--- apps/mobile-wallet/src/features/tokenDisplay/tokenDetailsModal/tokenDetailsModalTypes.ts
+++ apps/mobile-wallet/src/features/tokenDisplay/tokenDetailsModal/tokenDetailsModalTypes.ts
@@ -1,7 +1,13 @@
 import { AddressHash } from '@alephium/shared'
 import { Token } from '@alephium/web3'
 
+import { ModalInstance } from '~/features/modals/modalTypes'
+
 export interface TokenDetailsModalCommonProps {
   tokenId: Token['id']
   addressHash?: AddressHash
+}
+
+export interface TokenDetailsModalProps extends TokenDetailsModalCommonProps {
+  parentModalId?: ModalInstance['id']
 }
