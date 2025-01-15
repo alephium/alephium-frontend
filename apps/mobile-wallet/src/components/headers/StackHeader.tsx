83455b251460ef9ac6db58abf16ffce5e30e8460
mvaivre <m.vaivre@gmail.com>
2025-01-15 11:15:49 +0100

    Simplify send flow navigation, fixes issue with back btn

--- apps/mobile-wallet/src/components/headers/StackHeader.tsx
+++ apps/mobile-wallet/src/components/headers/StackHeader.tsx
@@ -1,13 +1,7 @@
-import { SceneProgress } from '@react-navigation/stack/lib/typescript/src/types'
-
 import Button from '~/components/buttons/Button'
 import BaseHeader, { BaseHeaderProps } from '~/components/headers/BaseHeader'
 
-export type StackHeaderCustomProps = BaseHeaderProps & {
-  progress?: SceneProgress
-}
-
-const StackHeader = ({ onBackPress: goBack, options, ...props }: StackHeaderCustomProps) => {
+const StackHeader = ({ onBackPress: goBack, options, ...props }: BaseHeaderProps) => {
   const HeaderLeft = goBack ? <Button onPress={goBack} iconProps={{ name: 'arrow-left' }} squared compact /> : null
 
   return <BaseHeader options={{ headerLeft: () => HeaderLeft, ...options }} {...props} />
