8e6b2cd982105ba75f14824f9ff6c99028b30294
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-01-16 19:19:23 +0800

    Detect WC URL in clipboard and pair dApp

--- apps/mobile-wallet/src/features/ecosystem/DAppWebViewScreen.tsx
+++ apps/mobile-wallet/src/features/ecosystem/DAppWebViewScreen.tsx
@@ -1,16 +1,24 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import * as Clipboard from 'expo-clipboard'
 import Constants from 'expo-constants'
+import { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
 import WebView from 'react-native-webview'
 import styled from 'styled-components/native'
 
 import AppText from '~/components/AppText'
 import Screen, { ScreenProps } from '~/components/layout/Screen'
+import { useWalletConnectContext } from '~/contexts/walletConnect/WalletConnectContext'
+import { activateAppLoading, deactivateAppLoading } from '~/features/loader/loaderActions'
+import { useAppDispatch } from '~/hooks/redux'
 import RootStackParamList from '~/navigation/rootStackRoutes'
 
 interface DAppWebViewScreenProps extends NativeStackScreenProps<RootStackParamList, 'DAppWebViewScreen'>, ScreenProps {}
 
 const DAppWebViewScreen = ({ navigation, route, ...props }: DAppWebViewScreenProps) => {
   const { dAppUrl } = route.params
+
+  useDetectWCUrlInClipboardAndPair()
 
   if (!dAppUrl) return null
 
@@ -22,6 +30,34 @@ const DAppWebViewScreen = ({ navigation, route, ...props }: DAppWebViewScreenPro
 }
 
 export default DAppWebViewScreen
+
+const useDetectWCUrlInClipboardAndPair = () => {
+  const dispatch = useAppDispatch()
+  const { t } = useTranslation()
+  const { pairWithDapp } = useWalletConnectContext()
+
+  useEffect(() => {
+    const checkClipboard = async () => {
+      const content = await Clipboard.getStringAsync()
+
+      if (content.startsWith('wc:')) {
+        Clipboard.setStringAsync('')
+
+        dispatch(activateAppLoading(t('Connecting')))
+
+        await pairWithDapp(content)
+
+        dispatch(deactivateAppLoading())
+      }
+    }
+
+    checkClipboard()
+
+    const intervalId = setInterval(checkClipboard, 1000)
+
+    return () => clearInterval(intervalId)
+  }, [dispatch, pairWithDapp, t])
+}
 
 const WebViewStyled = styled(WebView)`
   flex: 1;
