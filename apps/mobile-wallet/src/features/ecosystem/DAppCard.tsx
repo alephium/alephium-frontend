83c6fa66416200657615c10afbcbeb698c251a4d
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-01-16 21:49:00 +0800

    Improve dApp cards

--- apps/mobile-wallet/src/features/ecosystem/DAppCard.tsx
+++ apps/mobile-wallet/src/features/ecosystem/DAppCard.tsx
@@ -1,13 +1,15 @@
 import { useNavigation } from '@react-navigation/native'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { Image } from 'expo-image'
+import { Pressable } from 'react-native'
 import styled from 'styled-components/native'
 
 import AppText from '~/components/AppText'
 import { DApp } from '~/features/ecosystem/ecosystemTypes'
 import RootStackParamList from '~/navigation/rootStackRoutes'
+import { BORDER_RADIUS } from '~/style/globalStyle'
 
-const DAppCard = ({ name, media, links }: DApp) => {
+const DAppCard = ({ name, media, links, short_description }: DApp) => {
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>()
 
   const handleCardPress = () => {
@@ -16,22 +18,33 @@ const DAppCard = ({ name, media, links }: DApp) => {
 
   return (
     <CardContainer onPress={handleCardPress}>
-      <CardImage source={{ uri: media.bannerUrl }} contentFit="cover" />
-      <AppText>{name}</AppText>
+      <DappIcon source={{ uri: media.logoUrl }} contentFit="cover" />
+      <TextContent>
+        <AppText bold>{name}</AppText>
+        <AppText>{short_description}</AppText>
+      </TextContent>
     </CardContainer>
   )
 }
 
 export default DAppCard
 
-const CardContainer = styled.Pressable`
-  align-items: center;
-  background-color: ${({ theme }) => theme.bg.back1};
-  border-radius: 9px;
-  padding: 12px;
+const CardContainer = styled(Pressable)`
+  flex-direction: row;
+  overflow: hidden;
+  padding: 15px;
+  gap: 15px;
+  border-radius: ${BORDER_RADIUS}px;
+  background-color: ${({ theme }) => theme.bg.secondary};
 `
 
-const CardImage = styled(Image)`
-  height: 140px;
-  width: 100%;
+const DappIcon = styled(Image)`
+  height: 70px;
+  width: 70px;
+  border-radius: ${BORDER_RADIUS}px;
+`
+
+const TextContent = styled.View`
+  gap: 5px;
+  flex: 1;
 `
