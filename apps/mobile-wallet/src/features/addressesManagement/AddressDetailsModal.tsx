93119843c43e3ec8041c825da0c566420a8a5800
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-01-21 15:47:43 +0700

    Show empty placeholder

--- apps/mobile-wallet/src/features/addressesManagement/AddressDetailsModal.tsx
+++ apps/mobile-wallet/src/features/addressesManagement/AddressDetailsModal.tsx
@@ -1,15 +1,22 @@
 import { AddressHash } from '@alephium/shared'
 import { FlashList } from '@shopify/flash-list'
 import { useMemo } from 'react'
+import { useTranslation } from 'react-i18next'
 
 import AddressBadge from '~/components/AddressBadge'
 import { AddressesTokensListFooter } from '~/components/AddressesTokensList'
+import AppText from '~/components/AppText'
+import EmptyPlaceholder from '~/components/EmptyPlaceholder'
 import TokenListItem from '~/components/TokenListItem'
 import AddressDetailsModalHeader from '~/features/addressesManagement/AddressDetailsModalHeader'
 import BottomModalFlashList from '~/features/modals/BottomModalFlashList'
 import withModal from '~/features/modals/withModal'
 import { useAppSelector } from '~/hooks/redux'
-import { makeSelectAddressesKnownFungibleTokens } from '~/store/addresses/addressesSelectors'
+import {
+  makeSelectAddressesCheckedUnknownTokens,
+  makeSelectAddressesKnownFungibleTokens,
+  selectAddressHiddenAssetIds
+} from '~/store/addresses/addressesSelectors'
 
 export interface AddressDetailsModalProps {
   addressHash: AddressHash
@@ -29,6 +36,7 @@ const AddressDetailsModal = withModal<AddressDetailsModalProps>(({ id, addressHa
           estimatedItemSize={70}
           ListHeaderComponent={() => <AddressDetailsModalHeader addressHash={addressHash} parentModalId={id} />}
           ListFooterComponent={() => <AddressesTokensListFooter addressHash={addressHash} parentModalId={id} />}
+          ListEmptyComponent={() => <AddressesTokensListEmpty addressHash={addressHash} />}
           renderItem={({ item: entry, index }) => (
             <TokenListItem
               key={entry.id}
@@ -46,3 +54,32 @@ const AddressDetailsModal = withModal<AddressDetailsModalProps>(({ id, addressHa
 })
 
 export default AddressDetailsModal
+
+const AddressesTokensListEmpty = ({ addressHash }: { addressHash: AddressHash }) => {
+  const selectAddressesKnownFungibleTokens = useMemo(makeSelectAddressesKnownFungibleTokens, [])
+  const knownFungibleTokens = useAppSelector((s) => selectAddressesKnownFungibleTokens(s, addressHash, true))
+  const selectAddressesCheckedUnknownTokens = useMemo(makeSelectAddressesCheckedUnknownTokens, [])
+  const unknownTokens = useAppSelector(selectAddressesCheckedUnknownTokens)
+  const hiddenAssetIds = useAppSelector((s) => selectAddressHiddenAssetIds(s, addressHash))
+  const addressesBalancesStatus = useAppSelector((s) => s.addresses.balancesStatus)
+  const { t } = useTranslation()
+
+  const hasHiddenTokens = hiddenAssetIds.length > 0
+  const hasUnknownTokens = unknownTokens.length > 0
+
+  if (addressesBalancesStatus === 'uninitialized')
+    return (
+      <EmptyPlaceholder>
+        <AppText size={28}>⏳</AppText>
+        <AppText>{t('Loading your balances...')}</AppText>
+      </EmptyPlaceholder>
+    )
+
+  if (knownFungibleTokens.length === 0 && !hasUnknownTokens && !hasHiddenTokens)
+    return (
+      <EmptyPlaceholder>
+        <AppText size={28}>👀</AppText>
+        <AppText>{t('No assets here, yet.')}</AppText>
+      </EmptyPlaceholder>
+    )
+}
