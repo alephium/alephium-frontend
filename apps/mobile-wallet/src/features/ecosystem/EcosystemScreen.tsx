bbbef741afdfff7de52ff343fd063c8cb7af4bb5
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-01-23 11:46:43 +0700

    Revert "Enable ecosystem screen"
    This reverts commit 3e9777663067df4388adf625908d8bb39f415a9c.

--- apps/mobile-wallet/src/features/ecosystem/EcosystemScreen.tsx
+++ apps/mobile-wallet/src/features/ecosystem/EcosystemScreen.tsx
@@ -2,9 +2,14 @@ import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import AnimatedBackground from '~/components/AnimatedBackground'
+import AppText from '~/components/AppText'
+import EmptyPlaceholder from '~/components/EmptyPlaceholder'
 import BottomBarScrollScreen from '~/components/layout/BottomBarScrollScreen'
+import { ScreenSection } from '~/components/layout/Screen'
 import DAppsList from '~/features/ecosystem/DAppsList'
 import DAppsTags from '~/features/ecosystem/DAppsTags'
+
+const showComingSoon = true
 
 const EcosystemScreen = () => {
   const { t } = useTranslation()
@@ -20,9 +25,20 @@ const EcosystemScreen = () => {
       hasBottomBar
       fill
     >
-      <AnimatedBackground isFullScreen isAnimated />
-      <DAppsTags selectedTag={selectedTag} onTagPress={setSelectedTag} />
-      <DAppsList selectedTag={selectedTag} />
+      {showComingSoon ? (
+        <ScreenSection>
+          <EmptyPlaceholder>
+            <AppText size={28}>📣👀</AppText>
+            <AppText>{t('Coming soon!')}</AppText>
+          </EmptyPlaceholder>
+        </ScreenSection>
+      ) : (
+        <>
+          <AnimatedBackground isFullScreen isAnimated />
+          <DAppsTags selectedTag={selectedTag} onTagPress={setSelectedTag} />
+          <DAppsList selectedTag={selectedTag} />
+        </>
+      )}
     </BottomBarScrollScreen>
   )
 }
