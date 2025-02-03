05d40318664ea6c7c6d49ee1944c40c6d1286df8
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-02-03 11:06:09 +0100

    Add onramper client UUID

--- apps/mobile-wallet/src/features/buy/useOnramperUrl.ts
+++ apps/mobile-wallet/src/features/buy/useOnramperUrl.ts
@@ -2,6 +2,7 @@ import { AddressHash } from '@alephium/shared'
 import { useTheme } from 'styled-components/native'
 
 const ALPH_CODE = 'alph_alph'
+const ONRAMPER_CLIENT_UUID = 'pk_prod_01JJSAZY2D3XSZ614FKBC5EXYE'
 
 const useOnramperUrl = (receiveAddressHash: AddressHash) => {
   const theme = useTheme()
@@ -17,7 +18,8 @@ const useOnramperUrl = (receiveAddressHash: AddressHash) => {
     `&containerColor=${theme.bg.back1.slice(1)}` +
     `&primaryTextColor=${theme.font.primary.slice(1)}` +
     `&primaryColor=${theme.global.accent.slice(1)}` +
-    `&secondaryColor=${theme.global.complementary.slice(1)}`
+    `&secondaryColor=${theme.global.complementary.slice(1)}` +
+    `&apiKey=${ONRAMPER_CLIENT_UUID}`
   )
 }
 
