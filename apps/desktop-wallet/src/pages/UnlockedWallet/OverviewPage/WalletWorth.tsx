c1c86c362cb567aac6c537ffafad0c3f47acd147
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-02-05 14:36:01 +0100

    Remove unused prop

--- apps/desktop-wallet/src/pages/UnlockedWallet/OverviewPage/WalletWorth.tsx
+++ apps/desktop-wallet/src/pages/UnlockedWallet/OverviewPage/WalletWorth.tsx
@@ -3,14 +3,10 @@ import { memo } from 'react'
 import useFetchWalletWorth from '@/api/apiDataHooks/wallet/useFetchWalletWorth'
 import WorthOverview from '@/components/WorthOverview'
 
-interface WalletWorthProps {
-  overrideWorth?: number
-}
-
-const WalletWorth = memo((props: WalletWorthProps) => {
+const WalletWorth = memo(() => {
   const { data: worth, isLoading, isFetching, error } = useFetchWalletWorth()
 
-  return <WorthOverview worth={worth} isLoading={isLoading} isFetching={isFetching} error={error} {...props} />
+  return <WorthOverview worth={worth} isLoading={isLoading} isFetching={isFetching} error={error} />
 })
 
 export default WalletWorth
