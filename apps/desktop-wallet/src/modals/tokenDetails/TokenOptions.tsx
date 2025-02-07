5c3e164d496199e67576149cc85f9964a683049a
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-02-07 12:18:42 +0100

    Add link to contract address in explorer

--- apps/desktop-wallet/src/modals/tokenDetails/TokenOptions.tsx
+++ apps/desktop-wallet/src/modals/tokenDetails/TokenOptions.tsx
@@ -1,3 +1,4 @@
+import { addressFromTokenId } from '@alephium/web3'
 import { MoreVertical } from 'lucide-react'
 import { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
@@ -8,23 +9,21 @@ import { hideToken, unhideToken } from '@/features/hiddenTokens/hiddenTokensActi
 import { selectIsTokenHidden } from '@/features/hiddenTokens/hiddenTokensSelectors'
 import { useAppDispatch, useAppSelector } from '@/hooks/redux'
 import { TokenDetailsModalProps } from '@/modals/tokenDetails/tokeDetailsTypes'
+import { openInWebBrowser } from '@/utils/misc'
 
 const TokenDropdownOptions = ({ tokenId }: TokenDetailsModalProps) => {
   const { t } = useTranslation()
   const dispatch = useAppDispatch()
   const { sendAnalytics } = useAnalytics()
   const isTokenHidden = useAppSelector((s) => selectIsTokenHidden(s, tokenId))
-  // const explorerUrl = useAppSelector((s) => s.network.settings.explorerUrl)
+  const explorerUrl = useAppSelector((s) => s.network.settings.explorerUrl)
 
   const options: Array<DropdownOption> = useMemo(
     () => [
-      // {
-      //   label: t('Show in explorer'),
-      //   onClick: () => {
-      //     // TODO: Add link to explorer when explorer supports token page
-      //     openInWebBrowser(`${explorerUrl}/token/${tokenId}`)
-      //   }
-      // },
+      {
+        label: t('Show in explorer'),
+        onClick: () => openInWebBrowser(`${explorerUrl}/addresses/${addressFromTokenId(tokenId)}`)
+      },
       {
         label: t(isTokenHidden ? 'Unhide asset' : 'Hide asset'),
         onClick: () => {
@@ -33,7 +32,7 @@ const TokenDropdownOptions = ({ tokenId }: TokenDetailsModalProps) => {
         }
       }
     ],
-    [dispatch, isTokenHidden, sendAnalytics, t, tokenId]
+    [dispatch, explorerUrl, isTokenHidden, sendAnalytics, t, tokenId]
   )
 
   return <DropdownButton options={options} Icon={MoreVertical} circle tiny role="secondary" />
