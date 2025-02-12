7b2dbe31c73a047758e33dc8e190856b3738337b
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-02-12 12:33:32 +0100

    Fix no filtered tx message

--- apps/desktop-wallet/src/features/transactionsDisplay/transactionLists/TransactionsListFooter.tsx
+++ apps/desktop-wallet/src/features/transactionsDisplay/transactionLists/TransactionsListFooter.tsx
@@ -1,9 +1,12 @@
+import dayjs from 'dayjs'
 import { useTranslation } from 'react-i18next'
+import styled from 'styled-components'
 
 import ActionLink from '@/components/ActionLink'
 import PlaceholderText from '@/components/EmptyPlaceholder'
 import Spinner from '@/components/Spinner'
 import { TableCellPlaceholder, TableRow } from '@/components/Table'
+import { useAppSelector } from '@/hooks/redux'
 
 interface TransactionsListFooterBaseProps {
   isDisplayingTxs: boolean
@@ -15,23 +18,35 @@ interface InfiniteTransactionsListFooterProps extends TransactionsListFooterBase
   showLoadMoreBtn: boolean
   onShowMoreClick: () => void
   allTxsLoadedMsg: string
+  latestTxDate?: number
 }
 
 interface StaticTransactionsListFooterProps extends TransactionsListFooterBaseProps {
   showLoadMoreBtn: false
   onShowMoreClick?: undefined
   allTxsLoadedMsg?: undefined
+  latestTxDate?: undefined
 }
 
 const TransactionsListFooter = (props: InfiniteTransactionsListFooterProps | StaticTransactionsListFooterProps) => {
   const { t } = useTranslation()
+  const region = useAppSelector((state) => state.settings.region)
 
   if (isStaticTransactionsList(props) && props.isDisplayingTxs) return null
 
   return (
     <TableRow role="row" tabIndex={0}>
       <TableCellPlaceholder align="center">
-        {props.isDisplayingTxs ? (
+        {!props.isDisplayingTxs && props.showLoadMoreBtn && props.latestTxDate ? (
+          <NoTxsBeforeDate>
+            <PlaceholderTextStyled emoji="🔎">
+              {t('No transactions before {{ date }}', {
+                date: dayjs(props.latestTxDate).toDate().toLocaleString(region, { dateStyle: 'medium' })
+              })}
+            </PlaceholderTextStyled>
+            <ActionLink onClick={props.onShowMoreClick}>{t('Show more')}</ActionLink>
+          </NoTxsBeforeDate>
+        ) : props.isDisplayingTxs ? (
           props.showSpinner ? (
             <Spinner size="15px" />
           ) : props.showLoadMoreBtn ? (
@@ -53,3 +68,15 @@ const isStaticTransactionsList = (
   txList: InfiniteTransactionsListFooterProps | StaticTransactionsListFooterProps
 ): txList is StaticTransactionsListFooterProps =>
   (txList as StaticTransactionsListFooterProps).onShowMoreClick === undefined
+
+const NoTxsBeforeDate = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  gap: 10px;
+`
+
+const PlaceholderTextStyled = styled(PlaceholderText)`
+  width: 100%;
+`
