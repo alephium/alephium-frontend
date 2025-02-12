718aa20b310d6ee0f151665c0a113fc306b7c2fb
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-02-12 12:16:14 +0100

    Remove unused files

--- apps/desktop-wallet/src/api/context/apiContext.tsx
+++ apps/desktop-wallet/src/api/context/apiContext.tsx
@@ -4,15 +4,13 @@ import { UseFetchWalletBalancesAlphArrayContextProvider } from '@/api/apiDataHoo
 import { UseFetchWalletBalancesAlphByAddressContextProvider } from '@/api/apiDataHooks/wallet/useFetchWalletBalancesAlphByAddress'
 import { UseFetchWalletBalancesTokensArrayContextProvider } from '@/api/apiDataHooks/wallet/useFetchWalletBalancesTokensArray'
 import { UseFetchWalletBalancesTokensByAddressContextProvider } from '@/api/apiDataHooks/wallet/useFetchWalletBalancesTokensByAddress'
-import { UseFetchWalletTransactionsLimitedContextProvider } from '@/api/apiDataHooks/wallet/useFetchWalletTransactionsLimited'
 import { composeProviders } from '@/api/context/apiContextUtils'
 
 const Providers = composeProviders([
   UseFetchWalletBalancesTokensArrayContextProvider,
   UseFetchWalletBalancesTokensByAddressContextProvider,
   UseFetchWalletBalancesAlphArrayContextProvider,
-  UseFetchWalletBalancesAlphByAddressContextProvider,
-  UseFetchWalletTransactionsLimitedContextProvider
+  UseFetchWalletBalancesAlphByAddressContextProvider
 ])
 
 const ApiContextProvider = ({ children }: { children: ReactNode }) => <Providers>{children}</Providers>
