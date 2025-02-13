766cc154b6f9684591950ee1c6149fc475e75fa8
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-02-13 14:40:26 +0100

    Display cached NFT after tapping

--- apps/mobile-wallet/src/components/NFTImage.tsx
+++ apps/mobile-wallet/src/components/NFTImage.tsx
@@ -1,7 +1,7 @@
 import { NFT, selectNFTById } from '@alephium/shared'
 import { Image } from 'expo-image'
 import { CameraOff, FileImage } from 'lucide-react-native'
-import { memo, useEffect, useState } from 'react'
+import { memo, useState } from 'react'
 import { DimensionValue, Platform } from 'react-native'
 import styled, { useTheme } from 'styled-components/native'
 
@@ -9,6 +9,8 @@ import NftVideo from '~/components/nft/NftVideo'
 import NftVideoPlayIconOverlay from '~/components/nft/NftVideoPlayIconOverlay'
 import NFTPlaceholder from '~/components/NFTPlaceholder'
 import NFTWebView from '~/components/NFTWebView'
+import useIsNftCached from '~/features/assetsDisplay/nftsDisplay/useIsNftCached'
+import useNftHeaderData from '~/features/assetsDisplay/nftsDisplay/useNftHeaderData'
 import { useAppSelector } from '~/hooks/redux'
 import { BORDER_RADIUS_SMALL } from '~/style/globalStyle'
 
@@ -19,46 +21,23 @@ export interface NFTImageProps {
   sizeLimited?: boolean
 }
 
-enum NFTDataTypes {
-  image = 'image',
-  video = 'video',
-  audio = 'audio',
-  other = 'other'
-}
-
-type NFTDataType = keyof typeof NFTDataTypes
-
-const maxFileSizeInMB = 5
+const maxFileSizeInMB = 1
 const maxFileSizeInBytes = 1024 * 1024 * maxFileSizeInMB
 
 const NFTImage = memo(({ nftId, size = '100%', play, sizeLimited = true }: NFTImageProps) => {
   const nft = useAppSelector((s) => selectNFTById(s, nftId))
+  const isCached = useIsNftCached(nftId)
+  const { contentType, isLargeFile } = useNftHeaderData({ nftId, maxFileSizeInBytes })
   const theme = useTheme()
 
   const [hasError, setHasError] = useState(false)
-  const [contentType, setContentType] = useState<NFTDataType>()
-  const [isLargeFile, setIsLargeFile] = useState<boolean>()
-
-  useEffect(() => {
-    if (nft?.image)
-      fetch(nft.image, { method: 'HEAD' }).then((res) => {
-        const contentType = res.headers.get('content-type') || ''
-        const contentTypeCategory = contentType.split('/')[0]
-        const contentLength = res.headers.get('content-length')
-        const isLargeFile = contentLength ? parseInt(contentLength) > maxFileSizeInBytes : false
-
-        setIsLargeFile(isLargeFile)
-        setContentType(contentTypeCategory in NFTDataTypes ? (contentTypeCategory as NFTDataType) : 'other')
-      })
-  }, [nft?.image])
 
   if (!nft) return null
 
-  const isDataUri = nft.image.startsWith('data:image/')
-
   if (!contentType) return <NFTPlaceholder size={size} />
 
-  if (isLargeFile && sizeLimited) return <NFTPlaceholder size={size} Icon={FileImage} text={`>${maxFileSizeInMB}MB`} />
+  if (sizeLimited && !isCached && isLargeFile)
+    return <NFTPlaceholder size={size} Icon={FileImage} text={`>${maxFileSizeInMB}MB`} />
 
   // Loading many videos at once is too heavy on Android.
   // Using the image component for NFT lists on Android is sufficient.
@@ -66,6 +45,8 @@ const NFTImage = memo(({ nftId, size = '100%', play, sizeLimited = true }: NFTIm
     return <NftVideo size={size} videoSource={nft.image} play={play} />
 
   if (contentType === 'other' || hasError) return <NFTPlaceholder size={size} Icon={CameraOff} />
+
+  const isDataUri = nft.image.startsWith('data:image/')
 
   if (isDataUri) return <NFTWebView imageUri={nft.image} size={size} />
 
