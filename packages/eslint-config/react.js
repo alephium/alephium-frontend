1484dc93b0534469a8ebeecf3f029be74bce74dc
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-02-19 09:35:14 +0100

    Install React Compiler ESLint plugin

--- packages/eslint-config/react.js
+++ packages/eslint-config/react.js
@@ -4,7 +4,7 @@ const project = resolve(process.cwd(), 'tsconfig.json')
 
 module.exports = {
   extends: ['plugin:react/jsx-runtime'],
-  plugins: ['react', 'react-hooks'],
+  plugins: ['react', 'react-hooks', 'eslint-plugin-react-compiler'],
   rules: {
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
@@ -12,7 +12,8 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
     'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
     'jsx-quotes': [2, 'prefer-double'],
-    'react/jsx-curly-brace-presence': ['warn', { props: 'never', children: 'never' }]
+    'react/jsx-curly-brace-presence': ['warn', { props: 'never', children: 'never' }],
+    'react-compiler/react-compiler': 'warn'
   },
   parserOptions: {
     project
