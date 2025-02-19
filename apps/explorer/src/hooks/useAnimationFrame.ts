178b3a040743e2dd33be0c05b13293624aff6f09
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-02-19 10:41:22 +0100

    Fix some explorer React Compiler lint errors

--- apps/explorer/src/hooks/useAnimationFrame.ts
+++ apps/explorer/src/hooks/useAnimationFrame.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react'
+import { useCallback, useEffect, useRef } from 'react'
 
 const useAnimationFrame = (callback: (deltaTime: number) => void) => {
   // Use useRef for mutable variables that we want to persist
@@ -6,20 +6,23 @@ const useAnimationFrame = (callback: (deltaTime: number) => void) => {
   const requestRef = useRef<number>()
   const previousTimeRef = useRef<number>()
 
-  const animate = (time: number) => {
-    if (previousTimeRef.current != undefined) {
-      const deltaTime = time - previousTimeRef.current
-      callback(deltaTime)
-    }
-    previousTimeRef.current = time
-    requestRef.current = requestAnimationFrame(animate)
-  }
+  const animate = useCallback(
+    (time: number) => {
+      if (previousTimeRef.current != undefined) {
+        const deltaTime = time - previousTimeRef.current
+        callback(deltaTime)
+      }
+      previousTimeRef.current = time
+      requestRef.current = requestAnimationFrame(animate)
+    },
+    [callback]
+  )
 
   useEffect(() => {
     requestRef.current = requestAnimationFrame(animate)
+
     return () => cancelAnimationFrame(requestRef.current ?? 0)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []) // Make sure the effect runs only once
+  }, [animate]) // Make sure the effect runs only once
 }
 
 export default useAnimationFrame
