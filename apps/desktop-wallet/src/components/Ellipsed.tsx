c4a8e6379f37fa33c3aa742b7cbfe0c548ddee78
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-03-04 10:20:45 +0200

    Fix lint errors in middle ellipsed text

--- apps/desktop-wallet/src/components/Ellipsed.tsx
+++ apps/desktop-wallet/src/components/Ellipsed.tsx
@@ -1,7 +1,6 @@
-import { HTMLAttributes, useCallback, useEffect, useRef, useState } from 'react'
+import { useMiddleEllipsedText } from '@alephium/shared-react'
+import { HTMLAttributes } from 'react'
 import styled from 'styled-components'
-
-import { useWindowResize } from '@/utils/hooks'
 
 interface EllipsedProps extends HTMLAttributes<HTMLDivElement> {
   text: string
@@ -9,41 +8,19 @@ interface EllipsedProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 const Ellipsed = ({ text, className }: EllipsedProps) => {
-  const el = useRef<HTMLDivElement | null>(null)
-  const [_text, setText] = useState(text)
-
-  const handleResize = useCallback(() => {
-    if (el?.current === null) {
-      setText(text.substring(0, 5) + '...')
-    } else {
-      const visibleChars = Math.floor(el.current.clientWidth / (el.current.scrollWidth / text.length))
-      const half = visibleChars / 2
-
-      setText(
-        visibleChars >= text.length
-          ? text
-          : text.slice(0, Math.floor(half) - 2) +
-              (visibleChars === text.length ? '' : '...') +
-              text.slice(-Math.ceil(half) + 2)
-      )
-    }
-  }, [text])
-
-  useWindowResize(handleResize)
-
-  useEffect(() => {
-    handleResize()
-  }, [handleResize, text])
+  const { ellipsedText, ref } = useMiddleEllipsedText(text)
 
   return (
-    <div ref={el} className={className}>
+    <EllipsedStyled ref={ref} className={className}>
       <HiddenText>{text}</HiddenText>
-      <div>{_text}</div>
-    </div>
+      <div>{ellipsedText}</div>
+    </EllipsedStyled>
   )
 }
 
-export default styled(Ellipsed)`
+export default Ellipsed
+
+const EllipsedStyled = styled.div`
   font-family: 'Roboto Mono';
   overflow: hidden;
 `
