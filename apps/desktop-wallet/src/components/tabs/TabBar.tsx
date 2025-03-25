96b38028cc70ca076966c61582fcaf75a0add6d7
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-03-25 12:59:10 +0800

    Fix tabs glitch

--- apps/desktop-wallet/src/components/tabs/TabBar.tsx
+++ apps/desktop-wallet/src/components/tabs/TabBar.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import { ChevronRight } from 'lucide-react'
 import { useTranslation } from 'react-i18next'
 import styled, { css } from 'styled-components'
@@ -14,6 +15,8 @@ export interface TabBarProps<T extends string> {
   justifyTabs?: 'left' | 'center'
   TabComponent?: typeof Tab
   className?: string
+  onMouseOverTabBar?: (tabValue: T) => void
+  onMouseLeaveTabBar?: (tabValue: T) => void
 }
 
 const TabBar = <T extends string>({
@@ -24,7 +27,9 @@ const TabBar = <T extends string>({
   onLinkClick,
   TabComponent = Tab,
   justifyTabs = 'center',
-  className
+  className,
+  onMouseOverTabBar,
+  onMouseLeaveTabBar
 }: TabBarProps<T>) => {
   const { t } = useTranslation()
 
@@ -48,6 +53,8 @@ const TabBar = <T extends string>({
               tabIndex={0}
               aria-selected={isActive}
               isActive={isActive}
+              onMouseEnter={() => onMouseOverTabBar?.(item.value)}
+              onMouseLeave={() => onMouseLeaveTabBar?.(item.value)}
             >
               <TabLabel isActive={isActive}>
                 {item.Icon && (
@@ -85,7 +92,7 @@ const TabsContainer = styled.div`
   border-bottom: 1px solid ${({ theme }) => theme.border.primary};
 `
 
-export const Tab = styled.div<{ isActive: boolean }>`
+export const Tab = styled(motion.div)<{ isActive: boolean }>`
   display: flex;
   text-align: center;
   cursor: pointer;
