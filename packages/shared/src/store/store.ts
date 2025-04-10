c8c77c20a350aae11182c02f1101fd2b7e156d1d
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-04-10 13:53:28 +0200

    Get rid of NFTs slice

--- packages/shared/src/store/store.ts
+++ packages/shared/src/store/store.ts
@@ -3,13 +3,12 @@ import { AnyAction, Dispatch, ThunkDispatch } from '@reduxjs/toolkit'
 import addressesSlice from '@/store/addresses/addressesSlice'
 import fungibleTokensSlice from '@/store/assets/fungibleTokensSlice'
 import hiddenTokensSlice from '@/store/assets/hiddenTokensSlice'
-import nftsSlice from '@/store/assets/nftsSlice'
 import clientsSlice from '@/store/clients/clientsSlice'
 import networkSlice from '@/store/network/networkSlice'
 import pricesSlice from '@/store/prices/pricesSlice'
 import sentTransactionsSlice from '@/store/sentTransactions/sentTransactionsSlice'
 import { AddressesState, ClientsState, SentTransactionsState } from '@/types'
-import { FungibleTokensState, HiddenTokensState, NFTsState } from '@/types/assets'
+import { FungibleTokensState, HiddenTokensState } from '@/types/assets'
 import { NetworkState } from '@/types/network'
 import { PricesState } from '@/types/price'
 import { SharedSettings } from '@/types/sharedSettings'
@@ -19,7 +18,6 @@ import sharedSettingsSlice from './sharedSettings/sharedSettingsSlice'
 export const sharedReducer = {
   [pricesSlice.name]: pricesSlice.reducer,
   [fungibleTokensSlice.name]: fungibleTokensSlice.reducer,
-  [nftsSlice.name]: nftsSlice.reducer,
   [networkSlice.name]: networkSlice.reducer,
   [clientsSlice.name]: clientsSlice.reducer,
   [hiddenTokensSlice.name]: hiddenTokensSlice.reducer,
@@ -38,7 +36,6 @@ export const sharedReducer = {
 export type SharedRootState = {
   [pricesSlice.name]: PricesState
   [fungibleTokensSlice.name]: FungibleTokensState
-  [nftsSlice.name]: NFTsState
   [networkSlice.name]: NetworkState
   [clientsSlice.name]: ClientsState
   [hiddenTokensSlice.name]: HiddenTokensState
