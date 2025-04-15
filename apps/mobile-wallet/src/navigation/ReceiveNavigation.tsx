69b99745823ddc21d3df294bf381734c0df91979
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-04-15 17:07:07 +0200

    Upgrade react navigation deps to v7

--- apps/mobile-wallet/src/navigation/ReceiveNavigation.tsx
+++ apps/mobile-wallet/src/navigation/ReceiveNavigation.tsx
@@ -1,5 +1,10 @@
 import { AddressHash } from '@alephium/shared'
-import { NavigationContainer, ParamListBase, useNavigationContainerRef } from '@react-navigation/native'
+import {
+  NavigationContainer,
+  NavigationIndependentTree,
+  ParamListBase,
+  useNavigationContainerRef
+} from '@react-navigation/native'
 import { createStackNavigator, StackScreenProps } from '@react-navigation/stack'
 import { useTranslation } from 'react-i18next'
 import { View } from 'react-native'
@@ -29,17 +34,19 @@ const ReceiveNavigation = ({
     <HeaderContextProvider>
       <View style={{ flex: 1 }}>
         <ReceiveNavigationHeader onBackPress={handleGoBack} />
-        <NavigationContainer ref={navigationRef} independent>
-          <ReceiveStack.Navigator
-            screenOptions={{
-              headerShown: false
-            }}
-            initialRouteName="AddressScreen"
-          >
-            <ReceiveStack.Screen name="AddressScreen" component={AddressScreen} />
-            <ReceiveStack.Screen name="QRCodeScreen" component={QRCodeScreen} />
-          </ReceiveStack.Navigator>
-        </NavigationContainer>
+        <NavigationIndependentTree>
+          <NavigationContainer ref={navigationRef}>
+            <ReceiveStack.Navigator
+              screenOptions={{
+                headerShown: false
+              }}
+              initialRouteName="AddressScreen"
+            >
+              <ReceiveStack.Screen name="AddressScreen" component={AddressScreen} />
+              <ReceiveStack.Screen name="QRCodeScreen" component={QRCodeScreen} />
+            </ReceiveStack.Navigator>
+          </NavigationContainer>
+        </NavigationIndependentTree>
       </View>
     </HeaderContextProvider>
   )
