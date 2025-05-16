9fbb9923130110b91a1d77896ef3c8f376d00b24
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-05-16 13:46:55 +0200

    Delete prices slice

--- packages/shared/src/store/prices/pricesAdapter.ts
+++
@@ -1,8 +0,0 @@
-import { createEntityAdapter } from '@reduxjs/toolkit'
-
-import { TokenPriceEntity } from '@/types/price'
-
-export const tokenPricesAdapter = createEntityAdapter<TokenPriceEntity>({
-  selectId: (token) => token.symbol,
-  sortComparer: (a, b) => a.symbol.localeCompare(b.symbol)
-})
