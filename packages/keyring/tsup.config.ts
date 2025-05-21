9368de8c5e847682f2773d938d7c99d33e480620
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-05-21 15:14:36 +0200

    Temporarily polyfill buffer in keyring
    See https://github.com/alephium/alephium-web3/issues/504

--- packages/keyring/tsup.config.ts
+++ packages/keyring/tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, Options } from 'tsup'
 
 export default defineConfig((options: Options) => ({
   entry: {
+    'buffer-polyfill': 'src/buffer-polyfill.ts',
     index: 'src/index.ts'
   },
   clean: true,
