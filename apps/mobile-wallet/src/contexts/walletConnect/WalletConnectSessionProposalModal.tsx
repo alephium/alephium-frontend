ab2d5bc7a43297e95050b27d586bef2de9c91d5c
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-05-26 16:05:08 +0200

    Add comments

--- apps/mobile-wallet/src/contexts/walletConnect/WalletConnectSessionProposalModal.tsx
+++ apps/mobile-wallet/src/contexts/walletConnect/WalletConnectSessionProposalModal.tsx
@@ -88,7 +88,7 @@ const WalletConnectSessionProposalModal = withModal<WalletConnectSessionProposal
           ...keyring.generateAndCacheAddress({
             group,
             skipAddressIndexes: currentAddressIndexes.current,
-            keyType: 'default'
+            keyType: 'default' // TODO: Support groupless addresses
           }),
           label: '',
           color: getRandomLabelColor(),
