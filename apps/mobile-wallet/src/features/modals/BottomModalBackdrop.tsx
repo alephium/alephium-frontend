8cd06bc431beee3ce94654e476afb45fd832d14e
mvaivre <m.vaivre@gmail.com>
2025-05-26 16:39:41 +0200

    Fix modal's backdrop interpolation

--- apps/mobile-wallet/src/features/modals/BottomModalBackdrop.tsx
+++ apps/mobile-wallet/src/features/modals/BottomModalBackdrop.tsx
@@ -1,6 +1,7 @@
 import { BottomSheetBackdropProps } from '@gorhom/bottom-sheet'
 import { Dimensions, Pressable } from 'react-native'
 import Animated, { Extrapolation, interpolate, useAnimatedStyle } from 'react-native-reanimated'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import styled from 'styled-components/native'
 
 interface BottomModalBackdropProps extends BottomSheetBackdropProps {
@@ -9,11 +10,13 @@ interface BottomModalBackdropProps extends BottomSheetBackdropProps {
 
 const AnimatedPressable = Animated.createAnimatedComponent(Pressable)
 
-const BottomModalBackdrop = ({ animatedPosition, animatedIndex, style, onPress }: BottomModalBackdropProps) => {
+const BottomModalBackdrop = ({ animatedPosition, style, onPress }: BottomModalBackdropProps) => {
   const screenHeight = Dimensions.get('window').height
+  const { top, bottom } = useSafeAreaInsets()
+  const insetsSum = top + bottom
 
   const containerAnimatedStyle = useAnimatedStyle(() => ({
-    opacity: interpolate(animatedPosition.value, [screenHeight, 0], [0, 1], Extrapolation.CLAMP)
+    opacity: interpolate(animatedPosition.value, [screenHeight - insetsSum, 0], [0, 1], Extrapolation.CLAMP)
   }))
 
   return <Backdrop style={[style, containerAnimatedStyle]} onPress={onPress} />
