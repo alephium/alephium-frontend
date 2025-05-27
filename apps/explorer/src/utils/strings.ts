2f3ef14206c481248a5081b9440fe945e03a65d2
mvaivre <m.vaivre@gmail.com>
2025-05-27 15:05:52 +0200

    Use simple address / group split fn

--- apps/explorer/src/utils/strings.ts
+++ apps/explorer/src/utils/strings.ts
@@ -34,5 +34,3 @@ export const formatNumberForDisplay = (
 
 export const SIMPLE_DATE_FORMAT = 'DD/MM/YYYY'
 export const DATE_TIME_FORMAT = `${SIMPLE_DATE_FORMAT} HH:mm:ss [UTC]Z`
-
-export const removeGroupIndexFromAddress = (address: string) => address.replace(/:\d+$/, '')
