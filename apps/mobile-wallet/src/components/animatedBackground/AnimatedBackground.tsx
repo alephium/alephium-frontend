a11afaf273bfe53989ae6beeacb4e708b9d47b5f
mvaivre <m.vaivre@gmail.com>
2025-06-18 16:55:27 +0200

    Optimize animated bg (hide when not in focus)

--- apps/mobile-wallet/src/components/animatedBackground/AnimatedBackground.tsx
+++ apps/mobile-wallet/src/components/animatedBackground/AnimatedBackground.tsx
@@ -1,9 +1,17 @@
+import { useIsFocused } from '@react-navigation/native'
 import { Blur, Canvas, Fill, LinearGradient, RadialGradient, Rect, vec } from '@shopify/react-native-skia'
 import { colord } from 'colord'
 import { Group } from 'lucide-react-native'
-import { memo, useState } from 'react'
+import { memo, useEffect, useState } from 'react'
 import { LayoutChangeEvent } from 'react-native'
-import { SensorType, useAnimatedSensor, useDerivedValue, withSpring } from 'react-native-reanimated'
+import Animated, {
+  SensorType,
+  useAnimatedSensor,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withSpring
+} from 'react-native-reanimated'
 import styled, { useTheme } from 'styled-components/native'
 
 interface AnimatedBackgroundProps {
@@ -13,10 +21,21 @@ interface AnimatedBackgroundProps {
 
 const GYRO_MULTIPLIER = 100
 
+const springConfig = {
+  damping: 10,
+  stiffness: 100
+}
+
 const AnimatedBackground = memo(({ offsetTop = 0, shade }: AnimatedBackgroundProps) => {
   const theme = useTheme()
+  const isFocused = useIsFocused()
   const [containerDimensions, setContainerDimensions] = useState({ width: 0, height: 0 })
-  const gyroscope = useAnimatedSensor(SensorType.ROTATION)
+  const gyroscope = useAnimatedSensor(SensorType.ROTATION, { interval: isFocused ? 'auto' : 1000000 })
+  const opacity = useSharedValue(1)
+
+  useEffect(() => {
+    opacity.value = withSpring(isFocused ? 1 : 0, springConfig)
+  }, [isFocused, opacity])
 
   const linearGradientPositions = [0.1, 0.2, 0.6, 0.7, 0.75, 1]
   const radialGradientPositions = [0.4, 0.5, 0.6, 0.7, 0.75, 1]
@@ -39,10 +58,13 @@ const AnimatedBackground = memo(({ offsetTop = 0, shade }: AnimatedBackgroundPro
     setContainerDimensions({ width, height })
   }
 
-  const sinRoll = useDerivedValue(() => Math.sin(gyroscope?.sensor.get().roll || 0))
-  const sinZ = useDerivedValue(() => Math.sin(gyroscope?.sensor.get().pitch || 0))
+  const sinRoll = useDerivedValue(() => (isFocused ? Math.sin(gyroscope?.sensor.get().roll || 0) : 0))
+  const sinZ = useDerivedValue(() => (isFocused ? Math.sin(gyroscope?.sensor.get().pitch || 0) : 0))
 
   const radialGradientCenter = useDerivedValue(() => {
+    if (!isFocused) {
+      return vec(containerDimensions.width / 2, containerDimensions.height + 70)
+    }
     const x = containerDimensions.width / 2 + sinRoll.value * GYRO_MULTIPLIER
     const y = containerDimensions.height + 70
     return vec(x, y)
@@ -50,40 +72,55 @@ const AnimatedBackground = memo(({ offsetTop = 0, shade }: AnimatedBackgroundPro
 
   const radialGradientRadius = useDerivedValue(() => {
     const baseRadius = containerDimensions.width * 0.5
+    if (!isFocused) return baseRadius
 
     return withSpring(baseRadius * (1 - sinZ.value * 0.2))
   })
 
   const linearGradientEnd = useDerivedValue(() => {
+    if (!isFocused) {
+      return vec(containerDimensions.width, containerDimensions.height)
+    }
     const x = containerDimensions.width + sinRoll.value * GYRO_MULTIPLIER
     const y = containerDimensions.height * (1 + sinZ.value * 0.2)
     return vec(x, y)
   })
 
+  const animatedPlaceholderStyle = useAnimatedStyle(() => ({
+    opacity: 1 - opacity.value
+  }))
+
+  const animatedGradientStyle = useAnimatedStyle(() => ({
+    opacity: opacity.value
+  }))
+
   return (
     <Container onLayout={handleLayout} style={{ top: offsetTop }}>
-      <Canvas style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}>
-        <Group>
-          <Fill color={theme.bg.primary} />
-          <Rect x={0} y={0} width={containerDimensions.width} height={containerDimensions.height}>
-            <LinearGradient
-              colors={getGradientColors(1)}
-              positions={linearGradientPositions}
-              start={vec(0, 0)}
-              end={linearGradientEnd}
-            />
-          </Rect>
-          <Rect x={0} y={0} width={containerDimensions.width} height={containerDimensions.height} opacity={0.9}>
-            <RadialGradient
-              c={radialGradientCenter}
-              r={radialGradientRadius}
-              colors={getGradientColors(theme.name === 'light' ? 0.5 : 0.9)}
-              positions={radialGradientPositions}
-            />
-          </Rect>
-          <Blur blur={20} />
-        </Group>
-      </Canvas>
+      <AnimatedPlaceholderBackground style={[{ backgroundColor: theme.bg.primary }, animatedPlaceholderStyle]} />
+      <AnimatedGradientBackground style={animatedGradientStyle}>
+        <Canvas style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}>
+          <Group>
+            <Fill color={theme.bg.primary} />
+            <Rect x={0} y={0} width={containerDimensions.width} height={containerDimensions.height}>
+              <LinearGradient
+                colors={getGradientColors(1)}
+                positions={linearGradientPositions}
+                start={vec(0, 0)}
+                end={linearGradientEnd}
+              />
+            </Rect>
+            <Rect x={0} y={0} width={containerDimensions.width} height={containerDimensions.height} opacity={0.9}>
+              <RadialGradient
+                c={radialGradientCenter}
+                r={radialGradientRadius}
+                colors={getGradientColors(theme.name === 'light' ? 0.5 : 0.9)}
+                positions={radialGradientPositions}
+              />
+            </Rect>
+            <Blur blur={20} />
+          </Group>
+        </Canvas>
+      </AnimatedGradientBackground>
     </Container>
   )
 })
@@ -97,3 +134,22 @@ const Container = styled.View`
   left: 0;
   bottom: 0;
 `
+
+const SimpleBackground = styled.View`
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+`
+
+const GradientBackground = styled.View`
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+`
+
+const AnimatedPlaceholderBackground = Animated.createAnimatedComponent(SimpleBackground)
+const AnimatedGradientBackground = Animated.createAnimatedComponent(GradientBackground)
