b1f5301aafac69fe06d537c7e8b5ee2c25589a4a
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-06-23 17:31:44 +0300

    Cleanup

--- apps/mobile-wallet/src/features/modals/ModalContext.tsx
+++ apps/mobile-wallet/src/features/modals/ModalContext.tsx
@@ -9,19 +9,21 @@ interface ModalContextType {
   onUserDismiss?: () => void
 }
 
-export const ModalContext = createContext<ModalContextType | null>(null)
+const ModalContext = createContext<ModalContextType | null>(null)
 
 interface ModalContextProviderProps extends UseModalDismissProps {
   children: ReactNode
 }
 
-export const ModalContextProvider = ({ id, onUserDismiss, children }: ModalContextProviderProps) => {
+const ModalContextProvider = ({ id, onUserDismiss, children }: ModalContextProviderProps) => {
   const { dismissModal, onDismiss } = useModalDismiss({ id, onUserDismiss })
 
   return (
     <ModalContext.Provider value={{ id, dismissModal, onDismiss, onUserDismiss }}>{children}</ModalContext.Provider>
   )
 }
+
+export default ModalContextProvider
 
 export const useModalContext = () => {
   const context = useContext(ModalContext)
@@ -31,4 +33,10 @@ export const useModalContext = () => {
   }
 
   return context
+}
+
+export const usePotentialModalId = () => {
+  const context = useContext(ModalContext)
+
+  return context?.id
 }
