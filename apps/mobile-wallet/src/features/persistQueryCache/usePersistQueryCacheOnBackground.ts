34edd7ed81a0228c38706ddde9ce3b535a8bb627
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-07-30 14:32:02 +0200

    Remove logs

--- apps/mobile-wallet/src/features/persistQueryCache/usePersistQueryCacheOnBackground.ts
+++ apps/mobile-wallet/src/features/persistQueryCache/usePersistQueryCacheOnBackground.ts
@@ -1,27 +1,21 @@
 import { usePersistQueryClientContext } from '@alephium/shared-react'
-import { useEffect, useRef, useState } from 'react'
-import { AppState, AppStateStatus } from 'react-native'
+import { useEffect, useRef } from 'react'
+import { AppState } from 'react-native'
 
 import { useAppSelector } from '~/hooks/redux'
 
 const usePersistQueryCacheOnBackground = () => {
-  const { persistQueryCache } = usePersistQueryClientContext()
-
   const appState = useRef(AppState.currentState)
-  const [appStateVisible, setAppStateVisible] = useState<AppStateStatus>()
   const walletId = useAppSelector((s) => s.wallet.id)
+  const { persistQueryCache } = usePersistQueryClientContext()
 
   useEffect(() => {
     const subscription = AppState.addEventListener('change', (nextAppState) => {
       if (nextAppState.match(/inactive|background/) && appState.current === 'active') {
-        console.log('App has come to the background!')
         persistQueryCache(walletId)
       }
 
       appState.current = nextAppState
-      setAppStateVisible(appState.current)
-
-      console.log('AppState', appState.current)
     })
 
     return () => {
