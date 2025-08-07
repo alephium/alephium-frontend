cef433dca538d089e0c67ce40df33d8c474e49cb
mvaivre <m.vaivre@gmail.com>
2025-08-07 10:21:38 +0200

    Fix offline message padding

--- apps/desktop-wallet/src/features/offline/OfflineMessage.tsx
+++ apps/desktop-wallet/src/features/offline/OfflineMessage.tsx
@@ -16,34 +16,28 @@ const OfflineMessage = () => {
   const bothOffline = isNodeOffline && isExplorerOffline
 
   return (
-    <InfoBoxContainer>
-      <InfoBoxStyled Icon={WifiOff} importance={bothOffline ? 'alert' : 'warning'} align="left">
-        {bothOffline
-          ? t('There is an issue connecting to the node and explorer backend servers.')
-          : isExplorerOffline
-            ? t(
-                'The explorer backend is offline. You can still see your balances and send transactions but some data might be missing.'
-              )
-            : t('The node is offline. You can see your balances but you cannot send transactions.')}
-        <br />
-        {t('We apologize for the inconvenience.')}
-        <br />
-        <br />
-        <Trans t={t} i18nKey="offlineModalStatusPageMessage">
-          Check your network settings and the
-          <ActionLink onClick={() => openInWebBrowser(links.statusPage)}>Status page</ActionLink> for updates on this
-          issue.
-        </Trans>
-      </InfoBoxStyled>
-    </InfoBoxContainer>
+    <InfoBoxStyled Icon={WifiOff} importance={bothOffline ? 'alert' : 'warning'} align="left">
+      {bothOffline
+        ? t('There is an issue connecting to the node and explorer backend servers.')
+        : isExplorerOffline
+          ? t(
+              'The explorer backend is offline. You can still see your balances and send transactions but some data might be missing.'
+            )
+          : t('The node is offline. You can see your balances but you cannot send transactions.')}
+      <br />
+      {t('We apologize for the inconvenience.')}
+      <br />
+      <br />
+      <Trans t={t} i18nKey="offlineModalStatusPageMessage">
+        Check your network settings and the
+        <ActionLink onClick={() => openInWebBrowser(links.statusPage)}>Status page</ActionLink> for updates on this
+        issue.
+      </Trans>
+    </InfoBoxStyled>
   )
 }
 
 export default OfflineMessage
-
-const InfoBoxContainer = styled.div`
-  padding: 50px var(--spacing-2) var(--spacing-2) var(--spacing-2);
-`
 
 const InfoBoxStyled = styled(InfoBox)`
   margin-bottom: 0;
--- apps/desktop-wallet/src/features/offline/OfflineModal.tsx
+++ apps/desktop-wallet/src/features/offline/OfflineModal.tsx
@@ -1,5 +1,6 @@
 import { memo } from 'react'
 import { useTranslation } from 'react-i18next'
+import styled from 'styled-components'
 
 import { ModalBaseProp } from '@/features/modals/modalTypes'
 import OfflineMessage from '@/features/offline/OfflineMessage'
@@ -10,9 +11,15 @@ const OfflineModal = memo(({ id }: ModalBaseProp) => {
 
   return (
     <CenteredModal title={t('Degraded experience')} id={id} noPadding>
-      <OfflineMessage />
+      <OfflineMessageContainer>
+        <OfflineMessage />
+      </OfflineMessageContainer>
     </CenteredModal>
   )
 })
 
 export default OfflineModal
+
+const OfflineMessageContainer = styled.div`
+  padding: 50px var(--spacing-2) var(--spacing-2) var(--spacing-2);
+`
