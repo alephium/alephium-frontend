2b4722e167cbc288dff2d981fc26b880e213ae82
Ilias Trichopoulos <hlias.nop@gmail.com>
2026-02-24 13:42:58 +0200

    Remove caching

--- packages/shared-react/src/api/apiDataHooks/useFetchOnramperSignature.ts
+++ packages/shared-react/src/api/apiDataHooks/useFetchOnramperSignature.ts
@@ -1,16 +1,11 @@
-import { AddressHash, getOnramperSignContent, ONE_DAY_MS } from '@alephium/shared'
+import { AddressHash, getOnramperSignContent } from '@alephium/shared'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 
-import { getQueryConfig } from '@/api/apiUtils'
-import { useCurrentlyOnlineNetworkId } from '@/network/useCurrentlyOnlineNetworkId'
-
 export const useFetchOnramperSignature = (addressHash: AddressHash) => {
-  const networkId = useCurrentlyOnlineNetworkId()
-
   const { data, isLoading, error } = useQuery({
     queryKey: ['onramper-signature', addressHash],
-    ...getQueryConfig({ staleTime: ONE_DAY_MS, gcTime: ONE_DAY_MS, networkId }),
+
     queryFn: async () => {
       const { data } = await axios.post('https://onramper.alephium.org/sign', {
         data: getOnramperSignContent(addressHash)
