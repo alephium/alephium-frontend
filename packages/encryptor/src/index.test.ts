31fb2596ee4c5c7b10eda6454c903c871cc08394
Ilias Trichopoulos <hlias.nop@gmail.com>
2026-04-07 17:45:51 +0200

    Fix lint

--- packages/encryptor/src/index.test.ts
+++ packages/encryptor/src/index.test.ts
@@ -6,8 +6,9 @@ describe('hexToBase64', () => {
   })
 
   it('converts long hex strings', () => {
-    expect(hexToBase64('a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0'))
-      .toBe('obLD1OX2p7jJ0OHyo7TF1uf4qbDB0uP0pbbH2Onw')
+    expect(hexToBase64('a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0')).toBe(
+      'obLD1OX2p7jJ0OHyo7TF1uf4qbDB0uP0pbbH2Onw'
+    )
   })
 
   it('converts all zeros', () => {
