4454fd21e9979e5d1deb5233fc5726a6c1efbfd4
Ilias Trichopoulos <hlias.nop@gmail.com>
2026-04-22 13:59:34 +0200

    Integrate staking APR

--- apps/mobile-wallet/src/api/powfi.ts
+++ apps/mobile-wallet/src/api/powfi.ts
@@ -1,8 +1,11 @@
+import type { App } from '@alephium/powfi-backend'
 import { Powfi } from '@alephium/powfi-sdk'
+import { treaty } from '@elysiajs/eden'
 
 import { stakingSigner } from '~/features/staking/stakingSigner'
 
 export const powfiSdk = Powfi.load({ networkId: 'testnet', signer: stakingSigner })
+export const powfiBackend = treaty<App>(process.env.EXPO_PUBLIC_POWFI_BACKEND_HOST || 'http://localhost:4000')
 export const xAlphTokenId = powfiSdk.staking.getConfig().xAlphTokenId
 
 powfiSdk.setCurrentProviders()
---
+++ apps/mobile-wallet/src/screens/Staking/StakingApr.tsx
@@ -0,0 +1,29 @@
+import { useMemo } from 'react'
+import styled from 'styled-components/native'
+
+import AppText from '~/components/AppText'
+import { useFetchStakingStats } from '~/features/staking/hooks/useFetchStakingStats'
+import { useAppSelector } from '~/hooks/redux'
+import AmountSkeleton from '~/screens/Staking/AmountSkeleton'
+
+const StakingApr = () => {
+  const { data, isLoading } = useFetchStakingStats()
+  const region = useAppSelector((s) => s.settings.region)
+
+  const percentFormatter = useMemo(
+    () => new Intl.NumberFormat(region, { style: 'percent', maximumFractionDigits: 2 }),
+    [region]
+  )
+
+  if (isLoading) return <AmountSkeleton height={25} />
+
+  return <StakingAprStyled>{data?.apr ? percentFormatter.format(Number(data.apr) / 100) : '0.00%'}</StakingAprStyled>
+}
+
+export default StakingApr
+
+const StakingAprStyled = styled(AppText)`
+  font-size: 20px;
+  font-weight: 600;
+  color: white;
+`
