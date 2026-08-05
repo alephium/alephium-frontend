9dfb3c649f4d1d229a09dcec87547502b3a5d1e7
Ilias Trichopoulos <hlias.nop@gmail.com>
2026-08-05 15:26:27 +0200

    Add loading animation in wallet creation screen

--- apps/mobile-wallet/src/screens/new-wallet/CreatingWalletScreen.tsx
+++ apps/mobile-wallet/src/screens/new-wallet/CreatingWalletScreen.tsx
@@ -2,12 +2,21 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { isEnrolledAsync } from 'expo-local-authentication'
 import { useEffect, useRef } from 'react'
 import { useTranslation } from 'react-i18next'
-import styled from 'styled-components/native'
+import Animated, {
+  cancelAnimation,
+  Easing,
+  SharedValue,
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withTiming
+} from 'react-native-reanimated'
+import styled, { useTheme } from 'styled-components/native'
 
 import { sendAnalytics } from '~/analytics'
 import WalletLottieAnimation from '~/animations/lottie/WalletLottieAnimation'
+import AppText from '~/components/AppText'
 import Screen, { ScreenProps } from '~/components/layout/Screen'
-import CenteredInstructions from '~/components/text/CenteredInstructions'
 import { useAppSelector } from '~/hooks/redux'
 import useCreateWallet from '~/hooks/useCreateWallet'
 import RootStackParamList from '~/navigation/rootStackRoutes'
@@ -16,6 +25,7 @@ import { sleep } from '~/utils/misc'
 import { resetNavigation } from '~/utils/navigation'
 
 const MIN_VISIBLE_DURATION = 1500
+const ELLIPSIS_STEP_MS = 400
 
 interface CreatingWalletScreenProps
   extends NativeStackScreenProps<RootStackParamList, 'CreatingWalletScreen'>,
@@ -65,15 +75,72 @@ const CreatingWalletScreen = ({ navigation, ...props }: CreatingWalletScreenProp
       <AnimationContainer style={{ marginTop: 100 }}>
         <WalletLottieAnimation />
       </AnimationContainer>
-      <CenteredInstructions instructions={[{ text: t('Creating wallet'), type: 'primary' }]} stretch fontSize={19} />
+      <StatusContainer>
+        <StatusLabel bold size={19}>
+          {t('Creating wallet')}
+        </StatusLabel>
+        <AnimatedEllipsis />
+      </StatusContainer>
     </Screen>
   )
 }
 
 export default CreatingWalletScreen
 
+// Key generation blocks the JS thread, so this must run on the UI thread via Reanimated.
+const AnimatedEllipsis = () => {
+  const theme = useTheme()
+  const progress = useSharedValue(0)
+
+  useEffect(() => {
+    progress.value = withRepeat(withTiming(4, { duration: ELLIPSIS_STEP_MS * 4, easing: Easing.linear }), -1, false)
+
+    return () => cancelAnimation(progress)
+  }, [progress])
+
+  return (
+    <EllipsisRow>
+      {[0, 1, 2].map((index) => (
+        <EllipsisDot key={index} index={index} progress={progress} color={theme.font.secondary} />
+      ))}
+    </EllipsisRow>
+  )
+}
+
+const EllipsisDot = ({ index, progress, color }: { index: number; progress: SharedValue<number>; color: string }) => {
+  const animatedStyle = useAnimatedStyle(() => ({
+    opacity: Math.floor(progress.value) > index ? 1 : 0
+  }))
+
+  return (
+    <Animated.Text
+      style={[{ color, fontSize: 28, fontFamily: 'Geist-Bold', width: 10, textAlign: 'center' }, animatedStyle]}
+    >
+      .
+    </Animated.Text>
+  )
+}
+
 const AnimationContainer = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+`
+
+const StatusContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  margin: 0 8%;
+`
+
+const StatusLabel = styled(AppText)`
+  text-align: center;
+  margin-bottom: 4px;
+`
+
+const EllipsisRow = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  min-height: 32px;
 `
